8ebf87d5402ed2eed375326e443ea1d2cd0c8266
Augusto Coronel <anstrinn@disroot.org>
2024-12-01 11:26:13 -0300

    script: Add home dotfiles sync tool

---
+++ scripts/__home-sync.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+# Help Menu
+
+function help() {
+  echo "Home dotfiles sync tool"
+  echo "Usage: $0 [option]"
+  echo "Available options:"
+  echo "help                                   - Displays this message and exits"
+}
+
+function rsync_home() {
+  # $SCRIPTS is an user defined directory set in .bashenv
+  # $SCRIPTS is present in $PATH
+  # Originally $SCRIPTS contains user created scripts
+  rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
+  rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
+  rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
+}
+
+case "$1" in
+  "help")
+    help
+    ;;
+  "")
+    rsync_home
+    ;;
+esac
