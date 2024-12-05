4b59df8072599507ea38d6a14daff04dc9443c6b
Augusto Coronel <anstrinn@disroot.org>
2024-12-05 15:34:40 -0300

    config: Removes dotfiles sync
    The previous commit added a line to sync all files within dotfiles directory
    
    That change broke the granularity to sync specific files in .config directory. Thus have been removed.

--- scripts/__home-sync.sh
+++ scripts/__home-sync.sh
@@ -17,7 +17,6 @@ function rsync_home() {
   rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
   rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
   rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
-  rsync -av --delete $HOME/dotfiles/ $HOME/home/dotfiles/
 }
 
 case "$1" in
