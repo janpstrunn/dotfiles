11065d62a8f9d4d28581385b540538b381766cb9
Augusto Coronel <anstrinn@disroot.org>
2024-12-06 08:05:33 -0300

    script: Add dotfiles sync
    My previous attempt to sync dotfiles ended breaking the granularity in
    .config/.
    However, making a backup of dotfiles still relevant, thus I added an
    exclusion to .config/.

--- scripts/__home-sync.sh
+++ scripts/__home-sync.sh
@@ -17,6 +17,7 @@ function rsync_home() {
   rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
   rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
   rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
+  rsync -av --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/"
 }
 
 case "$1" in
