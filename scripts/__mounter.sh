7f38156ac12ffc3d6deaec581a4edbba73bee1ed
Augusto Coronel <anstrinn@disroot.org>
2024-12-10 20:00:29 -0300

    scripts: Split open option
    The open option previously was able to both create non existing
    directories in /mnt/ and open luks devices.
    
    This gives more surface to integrate with other tools, like pass.

--- scripts/__mounter.sh
+++ scripts/__mounter.sh
@@ -9,32 +9,45 @@ function help() {
   echo "close [drive_name]            - Closes luks device and umount it"
 }
 
+function adddrive() {
+  echo "Existing drives:"
+  ls /mnt/
+  read -p "Name your drive: " drivename
+  if [ ! -d "/mnt/$drivename" ]; then
+    echo "No directory found at /mnt/$drivename. Creating one now..."
+    sudo mkdir "/mnt/$drivename"
+    sudo chown "$USER:$USER" "/mnt/$drivename"
+    echo "Directory /mnt/$drivename created."
+    exit 1
+  elif [ -d "mnt/$drivename" ]; then
+    echo "Drive already exists!"
+    exit 0
+  fi
+}
+
 function mount() {
-  device=$(ls /dev | grep -E "sd[a-z]+[0-9]+$" | fzf --prompt "Choose a drive to mount")
+  device=$(ls /dev | grep -E "sd[a-z]+[0-9]+$" | fzf --prompt "Choose a drive to mount: ")
   echo "Previously mounted devices:" && ls /mnt/
   read -p "Name the drive: " drive
-  sudo cryptsetup luksOpen /dev/$device $drive && echo "$device has been opened and named as $drive!"
-  if [ ! -d "/mnt/$drive" ]; then
-      echo "No directory found at /mnt/$drive. Creating one now..."
-      sudo mkdir /mnt/$drive
-      echo "Directory /mnt/$drive created."
-  fi
-  sudo mount /dev/mapper/$drive /mnt/$drive/ && echo "$drive has been mounted to /mnt/$drive!"
-} 
+  sudo cryptsetup luksOpen "/dev/$device" "$drive" && echo "$device has been opened and named as $drive!"
+  sudo mount "/dev/mapper/$drive" "/mnt/$drive/" && echo "$drive has been mounted to /mnt/$drive!"
+}
 
 function umount() {
   drive=$(ls /dev/mapper/ | fzf --prompt "Choose a drive to umount: ")
-  sudo umount /mnt/$drive/
-  sudo cryptsetup luksClose $drive && echo "$drive has been umounted!"
+  sudo umount "/mnt/$drive/"
+  sudo cryptsetup luksClose "$drive" && echo "$drive has been umounted!"
 }
 
 function read_option() {
   while true; do
-  read -p "Choose an option (open/close): " option
-  if [ $option == "open" ]; then
+  read -p "Choose an option (add/open/close): " option
+  if [ "$option" == "open" ]; then
     mount
-  elif [ $option == "close" ]; then
+  elif [ "$option" == "close" ]; then
     umount
+  elif [ "$option" == "add" ]; then
+    adddrive
   else
     echo "Choose open or close!"
   fi
