f6ffb5293f28df8ea924fc808e208aaeedb20e31
Augusto Coronel <anstrinn@disroot.org>
2024-12-11 19:25:26 -0300

    scripts: Automate dir creation
    Now the script automatically creates a directory in /mnt/go if it
    doesn't exist already.
    When a tmux session is created, it attaches to the corresponding
    gocryptfs vault.

--- scripts/__unlocker.sh
+++ scripts/__unlocker.sh
@@ -8,11 +8,13 @@ function help() {
   echo "help                                   - Displays this message and exits"
 }
 
+vaultdir=$(ls "$VAULT")
+
 # Tmux Integration
 
 function cd-tmux() {
-  local SESSION_NAME=$(basename "/mnt/" | tr ' .:' '_')
-  tmux new-session -d -s "$SESSION_NAME" -c /mnt/
+  local SESSION_NAME=$(basename "/mnt/$govault" | tr ' .:' '_')
+  tmux new-session -d -s "$SESSION_NAME" -c "/mnt/$govault"
   tmux attach -t "$SESSION_NAME"
 }
 
@@ -22,10 +24,17 @@ function unlock() {
   # $VAULT is an user defined directory variable set in .bashenv
   # Originally $VAULT contains pgp encrypted files as .asc files
   # It also contains folders encrypted with gocryptfs
-  export vault=$(ls $VAULT/ | fzf --height 40% --prompt "Select vault to unlock: ")  
-  gocryptfs $VAULT/"$vault" /mnt/ &&
+  govault=$(echo "$vaultdir" | fzf --height 40% --prompt "Select vault to unlock: ")
+  if [ ! -d "/mnt/$govault" ]; then
+    echo "No directory found at /mnt/$govault. Creating one now..."
+    sudo mkdir "/mnt/$govault"
+    sudo chown "$USER:$USER" "/mnt/$govault"
+    echo "Directory /mnt/$govault created."
+  fi
+  gocryptfs "$VAULT/$govault" "/mnt/$govault" &&
   cd-tmux &&
-  fusermount3 -u /mnt/
+  fusermount3 -u "/mnt/$govault" && echo "$govault has been umounted successfully!"
+  sleep 1
   exit 0
 }
 
