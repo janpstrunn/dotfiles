6b62d3add9a14a7bea3476c4a310c36ed18ccee3
Janpstrunn <janpstrunn@particular.slmail.me>
2024-12-13 10:28:04 -0300

    scripts: Fix error redirect
    Redirecting the error to a full command execution was breaking the
    script when an entry has an otp. This blocked the urgent notification to
    appear, while the normal notification was working as expected.

--- scripts/__passmenu-otp.sh
+++ scripts/__passmenu-otp.sh
@@ -8,11 +8,15 @@ function help() {
   echo "help                                   - Displays this message and exits"
 }
 
+function error() {
+  notify-send -u normal "Selected pass entry doesn't contains an OTP." --expire-time=3000; exit 1
+}
+
 function passmenu-otp() {
   passdir=${PASSWORD_STORE_DIR:-$HOME/.password-store}
   pass=$(find "$passdir" -type f -name '*.gpg' -printf '%P\n' | awk -F. '{print $1}')
   password=$(echo "$pass" | rofi -dmenu "$@")
-  pass otp -c "$password" || notify-send -u normal "Selected pass entry doesn't contains an OTP." --expire-time=3000; exit 1
+  pass otp -c "$password" || error
   notify-send -u critical "OTP copied to clipboard. Clearing in 10 seconds" --expire-time=10000 --wait && echo "" | xclip -sel clip
 }
 
