f2e5e9a5dc4172a60007b15888a62ce3874ce2dd
Augusto Coronel <anstrinn@disroot.org>
2024-12-13 09:49:39 -0300

    scripts: Make clipboard temporary
    Now a notification will popup and will automatically close after 10
    seconds clearing the clipboard.

--- scripts/__passmenu-otp.sh
+++ scripts/__passmenu-otp.sh
@@ -13,6 +13,7 @@ function passmenu-otp() {
   pass=$(find "$passdir" -type f -name '*.gpg' -printf '%P\n' | awk -F. '{print $1}')
   password=$(echo "$pass" | rofi -dmenu "$@")
   pass otp -c "$password"
+  notify-send -u critical "OTP copied to clipboard. Clearing in 10 seconds" --expire-time=10000 --wait && echo "" | xclip -sel clip
 }
 
 case "$1" in
