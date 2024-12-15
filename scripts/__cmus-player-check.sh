eebce4d8ccdbb6a07da7c2ad0d6cc8e0fb3da116
Augusto Coronel <anstrinn@disroot.org>
2024-12-15 15:03:55 -0300

    scripts: Add cmus music toggle

---
+++ scripts/__cmus-player-check.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+function check_player() {
+  current_state=$(cmus-remote -Q | grep "status" | awk '{print $2}')
+  if [ "$current_state" = "playing" ]; then
+    cmus-remote -u
+  elif [ "$current_state" = "paused" ]; then
+    cmus-remote -p
+  else
+    echo "Cmus isn't playing or paused"
+  fi
+}
+
+check_player
