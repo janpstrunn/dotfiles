cf2666bdaf41a5f8f1eb5187899ff39cc9479e60
Augusto Coronel <anstrinn@disroot.org>
2024-12-19 15:16:34 -0300

    nvim: Add basic config files

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,60 @@
+local opt = vim.opt
+
+-- Tab / Indentation
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = true
+
+-- Search
+
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+
+-- Appearance
+
+opt.nu = true
+opt.relativenumber = true
+opt.termguicolors = true
+opt.colorcolumn = "100"
+opt.signcolumn = "yes"
+opt.cmdheight = 1
+opt.scrolloff = 10
+opt.completeopt = "menuone,noinsert,noselect"
+
+-- Behaviour
+
+opt.hidden = true
+opt.errorbells = false
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.backspace = "indent,eol,start"
+opt.splitright = true
+opt.splitbelow = true
+opt.autochdir = false
+opt.iskeyword:append("-")
+opt.mouse:append("a")
+opt.clipboard:append("unnamedplus")
+opt.modifiable = true
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- Spelling
+
+opt.spell = false
+opt.encoding = "UTF-8"
+opt.spelllang = "br,en_us"
+
+-- Folding
+
+opt.fillchars = { fold = " " }
+opt.foldmethod = "indent"
+opt.foldenable = false
+opt.foldlevel = 99
+vim.markdown_folding = 1
