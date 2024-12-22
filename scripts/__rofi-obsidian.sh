1dbd9a368b7e132518b7ddf195300299ff3d82bb
Janpstrunn <janpstrunn@disroot.org>
2024-12-22 17:26:29 -0300

    scripts: Include nvim

--- scripts/__rofi-obsidian.sh
+++ scripts/__rofi-obsidian.sh
@@ -1,29 +1,43 @@
 #!/bin/env bash
 
-# Help Menu
 function help() {
-  echo "Obsidian File Picker over Rofi"
-  echo "Usage: $0 [option]"
-  echo "Available options:"
-  echo "help                                   - Displays this message and exits"
+cat << EOF
+Quick Switcher via Rofi
+Usage: $0 [option]
+Available options:
+help                            - Displays this message and exits
+o                               - Open note in obsidian
+n                               - Open note in nvim
+EOF
 }
 
 if [ -z "$OBSIDIAN" ]; then
   source "$HOME/.env"
 fi
 
-function obsidian() {
-  # Originally $OBSIDIAN contain the path to an Obsidian Vault
+function open_nvim() {
   note=$(find "$OBSIDIAN" -type f -name '*.md' -printf '%P\n' | awk -F. '{print $1}')
   select=$(echo "$note" | rofi -dmenu -i "$@")
   kitty -e nvim "$OBSIDIAN/$select.md"
 }
 
+function open_obsidian() {
+  note=$(find "$OBSIDIAN" -type f -name '*.md' -printf '%P\n')
+  select=$(echo "$note" | rofi -dmenu -i "$@")
+  touch "$OBSIDIAN/$select"
+  obsidian-cli open "$select" --vault OUROBOROS
+}
+
 case "$1" in
-  "help")
+  "")
     help
     ;;
-  "")
-    obsidian
+  "n")
+    open_nvim
+    exit 0
+    ;;
+  "o")
+    open_obsidian
+    exit 0
     ;;
 esac
