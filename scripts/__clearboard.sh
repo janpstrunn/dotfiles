2a0df49efbf5df64f02c60ead2ffd7c5d8ce9083
Janpstrunn <janpstrunn@disroot.org>
2024-12-23 10:17:54 -0300

    scripts: Remove unnecessary code
    All scripts that contains help menus to display available arguments, but
    don't have any available are removed.
    Mostly unnecesary comments are removed.

--- scripts/__clearboard.sh
+++ scripts/__clearboard.sh
@@ -1,23 +1,8 @@
 #!/bin/env bash
 
-# Help Menu
-function help() {
-  echo "Clipboard clearer"
-  echo "Usage: $0 [option]"
-  echo "Available options:"
-  echo "help                                   - Displays this message and exits"
-}
-
 function clearboard() {
   dunstctl close-all
   echo "" | xclip -sel clip
 }
 
-case "$1" in
-  "help")
-    help
-    ;;
-  "")
-    clearboard
-    ;;
-esac
+clearboard
