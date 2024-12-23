17fc3ccde59c450714cd280fdce149d6e35c87c4
Janpstrunn <janpstrunn@disroot.org>
2024-12-23 10:17:54 -0300

    scripts: Remove unnecessary code
    All scripts that contains help menus to display available arguments, but
    don't have any available are removed.
    Mostly unnecesary comments are removed.

--- scripts/__clipass.sh
+++ scripts/__clipass.sh
@@ -1,30 +1,16 @@
 #!/bin/env bash
 
-# Help Menu
-
-function help() {
-  echo "Password Clipper"
-  echo "Usage: $0 [option]"
-  echo "Available options:"
-  echo "help                                   - Displays this message and exits"
-}
-
-vault="${VAULT:-$HOME/Vault/}"
+if [ -z "$VAULT" ]; then
+  source "$HOME/.env"
+fi
 
 function clipper() {
-  export pass=$(ls $vault | grep ".asc" | fzf --height 40% --prompt "Select pgp file to output: ")
+  export pass=$(ls $VAULT | grep ".asc" | fzf --height 40% --prompt "Select pgp file to output: ")
   if [ ! -f "$pass" ]; then
     echo "Exitting..."
     exit 1
   fi
-  gpg --decrypt $vault/$pass | xsel --clipboard --input --selectionTimeout 10000
+  gpg --decrypt $VAULT/$pass | xsel --clipboard --input --selectionTimeout 10000
 }
 
-case "$1" in
-  "help")
-    help
-    ;;
-  "")
-    clipper
-    ;;
-esac
+clipper
--- scripts/__passmenu-rofi.sh
+++ scripts/__passmenu-rofi.sh
@@ -1,13 +1,5 @@
 #!/bin/env bash
 
-# Help Menu
-function help() {
-  echo "A rofi passmenu"
-  echo "Usage: $0 [option]"
-  echo "Available options:"
-  echo "help                                   - Displays this message and exits"
-}
-
 function passmenu() {
   passdir=${PASSWORD_STORE_DIR:-$HOME/.password-store}
   pass=$(find "$passdir" -type f -name '*.gpg' -printf '%P\n' | awk -F. '{print $1}')
@@ -16,11 +8,4 @@ function passmenu() {
   notify-send -u critical "Password copied to clipboard. Clearing in 10 seconds" --expire-time=10000 --wait && echo "" | xclip -sel clip
 }
 
-case "$1" in
-  "help")
-    help
-    ;;
-  "")
-    passmenu
-    ;;
-esac
+passmenu
