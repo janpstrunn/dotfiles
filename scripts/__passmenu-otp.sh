e64315144a3f74eaa035b2ce042e1bf7f542d12e
Janpstrunn <janpstrunn@disroot.org>
2024-12-23 10:17:54 -0300

    scripts: Remove unnecessary code
    All scripts that contains help menus to display available arguments, but
    don't have any available are removed.
    Mostly unnecesary comments are removed.

--- scripts/__passmenu-otp.sh
+++ scripts/__passmenu-otp.sh
@@ -1,13 +1,5 @@
 #!/bin/env bash
 
-# Help Menu
-function help() {
-  echo "A passmenu-like for otp"
-  echo "Usage: $0 [option]"
-  echo "Available options:"
-  echo "help                                   - Displays this message and exits"
-}
-
 function error() {
   notify-send -u normal "Selected pass entry doesn't contains an OTP." --expire-time=3000; exit 1
 }
@@ -20,11 +12,4 @@ function passmenu-otp() {
   notify-send -u critical "OTP copied to clipboard. Clearing in 10 seconds" --expire-time=10000 --wait && echo "" | xclip -sel clip
 }
 
-case "$1" in
-  "help")
-    help
-    ;;
-  "")
-    passmenu-otp
-    ;;
-esac
+passmenu-otp
