d56ac45c470ea353378e1f46a79d74f019ae7967
Augusto Coronel <anstrinn@disroot.org>
2024-12-26 20:05:57 -0300

    refactor: Get image type by an array

--- scripts/__convert-avif.sh
+++ scripts/__convert-avif.sh
@@ -1,7 +1,15 @@
 #!/bin/env bash
 
-find . $ -iname '*.jpg' -o -iname '*.png' -o -iname '*.webp' $ -exec sh -c '
-  for img; do
-    magick -verbose "$img" "${img%.*}.avif" && rm "$img"
-  done
-' _ {} +
+imageformats=("jpg" "png" "webp")
+
+for format in "${imageformats[@]}"; do
+    find . -type f -iname "*.$format" | while read -r image_file; do
+        avif_file="${image_file%.$format}.avif"
+
+        magick "$image_file" "$avif_file"
+
+        rm "$image_file"
+
+        echo "Converted: $image_file to $avif_file"
+    done
+done
