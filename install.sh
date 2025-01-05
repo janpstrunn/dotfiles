c53bf323f7aa1655d3aa6b6753da2328f2dc6707
Augusto Coronel <anstrinn@disroot.org>
2025-01-05 09:03:30 -0300

    refactor: Remove several pacman commands
    Separate packages by type, but ran in a single pacman command to reduce
    user prompting.

--- install.sh
+++ install.sh
@@ -2,13 +2,27 @@
 
 set -eo pipefail
 
+essentials="rsync gocryptfs pass pass-otp gvfs mtpfs gvfs-mtp tmux ffmpeg polkit udiskie udisks2 fuse2 kitty mpv fastfetch onefetch unzip zip python-psutil ranger picom scrcpy lynx ddgr nsxiv bpytop direnv eza fd fzf github-cli gnome-keyring networkmanager-openvpn tldr man xwallpaper lazygit task taskwarrior-tui zoxide ripgrep xclip bat cmatrix figlet tmuxp cmus gitleaks moreutils redshift python-mutagen atomicparsley yt-dlp newsboat sxhkd navi imagemagick just less openssl-1.1 neomutt syncthing rustup zsh neovim"
+graphical="seahorse obsidian libreoffice-fresh shotcut handbrake flameshot gnome-disk-utility obs-studio gimp rofi dmenu dunst neovide"
+lsp="luarocks"
+tor="tor nyx"
+
 user=$(whoami)
 
+echo "→ upgrade and update pacman packages"
+
+sudo pacman -Sy
+sudo pacman -Syu
+
+echo "→ installing must have packages"
+
+sudo pacman -S stow git ufw
+
+echo "→ setting up firewall"
+
+sudo ufw enable
+
 echo "→ setting up dotfiles"
-
-echo "→ installing stow"
-
-sudo pacman -S stow
 
 cd "$HOME/dotfiles"
 stow .
@@ -17,8 +31,6 @@ cd
 GPG_KEY=$1
 
 echo "→ setting up git"
-
-sudo pacman -S git
 
 if [ -z "$NAME" ]; then
 	read -r -p "Please enter your git user.name" NAME
@@ -39,28 +51,6 @@ fi
 git config --global user.name "$NAME"
 git config --global user.email "$EMAIL"
 
-echo "→ upgrade and update pacman packages"
-
-sudo pacman -Sy
-sudo pacman -Syu
-
-# echo "→ setting up nextdns"
-
-# sh -c "$(curl -sL https://nextdns.io/install)"
-
-echo "→ setting up firewall"
-
-sudo pacman -S ufw
-
-sudo ufw enable
-
-# echo "→ setting up flatpak"
-
-# sudo pacman -S flatpak
-# flatpak install io.github.zen_browser.zen
-# flatpak install com.github.tchx84.Flatseal
-# flatpak install org.kde.okular
-
 echo "→ setting up yay"
 
 sudo pacman -S --needed git base-devel
@@ -69,45 +59,17 @@ cd yay
 makepkg -si
 cd
 
-echo "→ install essencial packages"
+echo "→ install pacman packages"
 
-sudo pacman -S rsync gocryptfs pass pass-otp gvfs mtpfs gvfs-mtp tmux ffmpeg polkit udiskie udisks2 fuse2 kitty mpv fastfetch onefetch unzip zip python-psutil ranger picom scrcpy lynx ddgr nsxiv bpytop direnv eza fd fzf github-cli gnome-keyring networkmanager-openvpn tldr man xwallpaper lazygit task taskwarrior-tui zoxide ripgrep xclip bat cmatrix figlet tmuxp cmus gitleaks moreutils redshift python-mutagen atomicparsley yt-dlp newsboat sxhkd navi imagemagick just less openssl-1.1
-
-# sudo pacman -S neomutt
-
-echo "→ install graphical packages"
-
-sudo pacman -S seahorse obsidian libreoffice-fresh shotcut handbrake flameshot gnome-disk-utility obs-studio gimp rofi dmenu dunst neovide
-
-# Other packages
-# sudo pacman -S audacity curtail
+sudo pacman -S "$essentials" "$graphical" "$tor" "$lsp"
 
 echo "→ install AUR packages"
 
-yay -S qtile-extras
-yay -S pet-bin
-yay -S buku
-yay -S zen-browser-bin
-yay -S obsidian-cli-bin
-yay -S cava
-
-echo "→ install LSP"
-
-sudo pacman -S luarocks
-
-echo "→ install Tor packages"
-
-sudo pacman -S tor nyx
-yay -S obfs4proxy
+yay -S qtile-extras pet-bin buku zen-browser-bin obsidian-cli-bin cava mutt-wizard obfs4proxy
 
 echo "→ setting up syncthing"
 
-sudo pacman -S syncthing
 sudo systemctl enable syncthing@$USER.service
-
-# echo "→ install pip"
-#
-# sudo pacman -S python3-pip
 
 echo "→ install fonts"
 
@@ -118,23 +80,6 @@ sudo pacman -S noto-fonts-emoji gnu-free-fonts ttf-ubuntu-nerd ttf-dejavu-nerd t
 # removing the necessity to install this every fresh install
 # yay -S ttf-ms-win10-auto
 
-# echo "→ install cargo"
-#
-# sudo pacman -S cargo
-
-# echo "→ install PipeWire"
-#
-# sudo pacman -S pipewire
-
-echo "→ install zsh and oh-my-posh"
-
-sudo pacman -S zsh
-curl -s https://ohmyposh.dev/install.sh | bash -s
-
-echo "→ installing Neovim"
-
-sudo pacman -S neovim
-
 echo "→ setting zsh as default shell"
 
 cd "$HOME"
@@ -142,12 +87,17 @@ sudo chsh -s "$(which zsh)" "$user"
 zsh
 source "$HOME/.zshrc"
 
+echo "→ install oh-my-posh"
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
 echo "→ installation complete"
 
 echo "You may now reboot to apply some packages and fonts"
 
 echo "→ How to apply final configurations"
 
-echo "picom --config "$HOME/.config/picom.conf""
-echo "ohmyposh --config "$HOME/.config/ohmyposh/""
-echo "kitty --config "$HOME/.config/kitty/kitty.conf""
+echo "just config-picom"
+echo "just config-posh"
+echo "just config-kitty"
+echo "just config-udiskie"
