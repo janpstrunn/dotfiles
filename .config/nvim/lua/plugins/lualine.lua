c83f70aedb8584cdb6c51e14f51f84bd3a32c2fb
Augusto Coronel <anstrinn@disroot.org>
2025-01-07 18:04:19 -0300

    nvim: Change separator symbol
    Better visual compatibility with tmux minimal status bar plugin.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -35,8 +35,8 @@ local config = function()
     options = {
       icons_enabled = true,
       theme = theme,
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
+      component_separators = { left = '|', right = '|'},
+      section_separators = { left = '', right = ''},
       disabled_filetypes = {
         statusline = {},
         winbar = {},
