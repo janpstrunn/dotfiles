f85012589b28bd5a1499cebc4a002aab3aa2ec0c
Augusto Coronel <anstrinn@disroot.org>
2025-01-10 10:57:30 -0300

    scripts: Add batch media converter

---
+++ scripts/__batch-media-converter.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+media_list=("ogg" "opus" "mp3" "mp4" "mkv" "webm" "m4a")
+
+echo "Media files in the current folder:"
+
+for media in "${media_list[@]}"; do
+  find . -maxdepth 1 -type f -iname "*.$media";
+done
+
+read -p "Choose source format: " source
+read -p "Choose wanted format: " dest
+
+for file in *."$source"; do
+  output="${file%.*}.$dest"
+  ffmpeg -i "$file" "$output"
+  echo "Converted: $file to $output"
+done
