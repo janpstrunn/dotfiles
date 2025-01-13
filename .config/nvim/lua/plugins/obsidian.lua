a2e9143827e916b5bf91322935cb0db22613e99f
Augusto Coronel <anstrinn@disroot.org>
2025-01-13 15:19:03 -0300

    nvim: Add new obsidian vault, and remove id and alias frontmatter

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -13,7 +13,11 @@ return {
         path = "/mnt/beelzebub/Pandora/obsidian/OUROBOROS",
       },
       {
-        name = "work",
+        name = "tree",
+        path = "/mnt/beelzebub/Pandora/obsidian/YGGDRASIL/",
+      },
+      {
+        name = "programming",
         path = "/mnt/beelzebub/Pandora/obsidian/HEPHAESTUS/",
       },
     },
@@ -44,7 +48,7 @@ return {
     }
   },
 
-  new_notes_location = "Ideas",
+  new_notes_location = "",
 
   wiki_link_func = function(opts)
     return require("obsidian.util").wiki_link_id_prefix(opts)
@@ -56,10 +60,7 @@ return {
   disable_frontmatter = false,
   ---@return table
   note_frontmatter_func = function(note)
-    if note.title then
-      note:add_alias(note.title)
-    end
-    local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+    local out = { tags = note.tags }
     if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
       for k, v in pairs(note.metadata) do
         out[k] = v
