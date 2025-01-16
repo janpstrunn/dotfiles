ecabd7aac16c69e89eadbfeeaf488ee517e6c281
Janpstrunn <janpstrunn@particular.slmail.me>
2025-01-16 14:26:34 -0300

    nvim: Disable oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,6 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
   opts = {},
-  -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  enabled = false,
 }
