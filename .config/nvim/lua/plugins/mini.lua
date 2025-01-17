de880d64600a914c7c5e2b481243550391376bd7
Janpstrunn <janpstrunn@particular.slmail.me>
2025-01-17 19:28:13 -0300

    nvim: Remove align and surround, add mini.nvim

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,8 @@
+local config = function()
+  require('mini.align').setup()
+end
+
+return {
+ 'echasnovski/mini.nvim', version = false,
+  config = config,
+}
--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,6 +1,7 @@
 return {
     "kylechui/nvim-surround",
     version = "*",
+    enabled = false,
     event = "VeryLazy",
     config = function()
         require("nvim-surround").setup({
