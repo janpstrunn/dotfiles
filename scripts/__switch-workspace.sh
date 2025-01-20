95895fb14dbcf1174bfd757da74ffb91c146a23e
Janpstrunn <janpstrunn@disroot.org>
2025-01-20 08:04:26 -0300

    feat(error): Add error handling for empty selection

--- scripts/__switch-workspace.sh
+++ scripts/__switch-workspace.sh
@@ -2,6 +2,10 @@
 
 function switch_workspace() {
   choose=$(ls $OBSIDIAN | rofi -dmenu)
+  if [ "$choose" = "" ]; then
+    notify-send -u low "No vault has been selected"
+    exit
+  fi
   echo "$OBSIDIAN/$choose" > "$HOME/.config/obsidian-workspace.conf"
 }
 
