1f47b024804f017fe6dba4905fff803c65b98cfc
Janpstrunn <janpstrunn@particular.slmail.me>
2025-01-24 08:45:54 -0300

    fix: Prevent root file ownership

--- scripts/__home-sync.sh
+++ scripts/__home-sync.sh
@@ -8,10 +8,10 @@ function rsync_base() {
 }
 
 function rsync_root() {
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
-  rsync --progress -avh --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/"
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/" && sudo chown -R $USER:$USER "$HOME/";
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/" && sudo chown -R $USER:$USER "$HOME/.config";
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/" && sudo chown -R $USER:$USER "$HOME/.local/share";
+  rsync --progress -avh --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/" && sudo chown -R $USER:$USER "$HOME/dotfiles"
 }
 
 case "$1" in
