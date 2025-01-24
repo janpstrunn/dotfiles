31036592703f9678642df2cd083a938193105ce0
Janpstrunn <janpstrunn@disroot.org>
2025-01-24 08:39:14 -0300

    feat: Set terminal as TERM environment variable

--- scripts/__tmuxp-open.sh
+++ scripts/__tmuxp-open.sh
@@ -5,10 +5,10 @@ function tp() {
   select=$(echo "$session" | rofi -dmenu)
   if [[ -n "$select" ]]; then
     if tmux has-session -t "$select"; then
-      ghostty -e tmux attach-session -t "$select"
+      $TERM -e tmux attach-session -t "$select"
     else
       tmuxp load "$select" -d
-      ghostty -e tmux attach-session -t "$select"
+      $TERM -e tmux attach-session -t "$select"
     fi
   fi
 }
