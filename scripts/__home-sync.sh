b2879df3a825f3f29c84dda44e288f3bebfdf3da
Janpstrunn <janpstrunn@particular.slmail.me>
2025-01-24 08:31:01 -0300

    feat: Allow running with sudo, show progress as human-readable

--- scripts/__home-sync.sh
+++ scripts/__home-sync.sh
@@ -1,10 +1,24 @@
 #!/bin/env bash
 
-function rsync_home() {
-  rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
-  rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
-  rsync -av --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
-  rsync -av --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/"
+function rsync_base() {
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
+  rsync --progress -avh --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/"
 }
 
-rsync_home
+function rsync_root() {
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
+  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
+  rsync --progress -avh --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/"
+}
+
+case "$1" in
+  "base")
+    rsync_base
+    ;;
+  "root")
+    rsync_root
+    ;;
+esac
