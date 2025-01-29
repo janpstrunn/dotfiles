4b92cf7b29da3906c2e3a4c75303dfad7c6f1ea7
Janpstrunn <janpstrunn@disroot.org>
2025-01-29 13:53:21 -0300

    feat: Add compatibility with wayland

--- scripts/__clearboard.sh
+++ scripts/__clearboard.sh
@@ -1,8 +1,14 @@
 #!/bin/env bash
 
+clipmethod=$(echo "$XDG_SESSION_TYPE")
+
 function clearboard() {
   dunstctl close-all
-  echo "" | xclip -sel clip
+  if [ "$clipmethod" = "x11" ]; then
+    echo "" | xclip -sel clip
+  else
+    echo "" | wl-copy
+  fi
 }
 
 clearboard
