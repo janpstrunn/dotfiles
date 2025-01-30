5b53baacd52a655d5c2880fb4a2e4173e87542a6
Augusto Coronel <anstrinn@disroot.org>
2025-01-30 18:49:47 -0300

    nvim: Add autoformat

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,16 +1,29 @@
 -- Remove trailing whitespace from all lines on save
 
 local CleanOnSave = vim.api.nvim_create_augroup('CleanOnSave', {})
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = CleanOnSave,
   pattern = "*",
   command = [[%s/\s\+$//e]],
 })
 
+-- Set colorscheme
 vim.api.nvim_create_augroup("ChangeColorscheme", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
   group = "ChangeColorscheme",
   callback = function()
-      vim.cmd("colorscheme catppuccin-mocha")
+    vim.cmd("colorscheme catppuccin-mocha")
   end,
+})
+
+-- Format file on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    local mode = vim.api.nvim_get_mode().mode
+    local filetype = vim.bo.filetype
+    if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+      vim.cmd('lua vim.lsp.buf.format()')
+    else
+    end
+  end
 })
