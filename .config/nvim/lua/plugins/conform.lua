263abec269235b8cda8c7aa73474fe7d7f974e3f
Janpstrunn <janpstrunn@disroot.org>
2025-02-02 17:09:05 -0300

    nvim: Add conform and footnote plugins, and new autocmds
    Autoformat and organize markdown footnotes on save, buffleave and focus
    loss.

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			sh = { "shfmt" },
+			bash = { "shfmt" },
+			zsh = { "shfmt" },
+			-- python = { "black" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			json = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			markdown = { "prettier" },
+		},
+	},
+}
