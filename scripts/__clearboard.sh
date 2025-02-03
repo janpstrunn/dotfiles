ab2d323126378eacd9288e4e1f14824deeb8bd32
Augusto Coronel <anstrinn@disroot.org>
2025-02-03 08:31:57 -0300

    feat: Add dunst close for wayland

--- scripts/__clearboard.sh
+++ scripts/__clearboard.sh
@@ -3,13 +3,14 @@
 clipmethod=$(echo "$XDG_SESSION_TYPE")
 
 function clearboard() {
-  if [ "$clipmethod" = "x11" ]; then
-    dunstctl close-all
-    echo "" | xclip -sel clip
-  elif [ "$clipmethod" = "wayland" ]; then
-    cliphist wipe
-    echo "" | wl-copy
-  fi
+	if [ "$clipmethod" = "x11" ]; then
+		dunstctl close-all
+		echo "" | xclip -sel clip
+	elif [ "$clipmethod" = "wayland" ]; then
+		cliphist wipe
+		dunstctl close-all
+		echo "" | wl-copy
+	fi
 }
 
 clearboard
