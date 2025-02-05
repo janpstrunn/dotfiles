f302ce50372281f25d374e8c9a971c8551381b6b
Janpstrunn <janpstrunn@disroot.org>
2025-02-05 15:01:22 -0300

    nvim: Add toggle wrap binding

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -525,3 +525,19 @@ local function blank_lines_markdown()
 end
 
 vim.keymap.set("n", "<leader>fmb", blank_lines_markdown, { desc = "Remove [B]lank Lines" })
+
+-- Toggle Wrap
+
+local function toggle_wrap()
+	if vim.wo.wrap then
+		vim.wo.wrap = false
+		vim.wo.linebreak = false
+		print("Wrap Disabled")
+	else
+		vim.wo.wrap = true
+		vim.wo.linebreak = true
+		print("Wrap Enabled")
+	end
+end
+
+vim.keymap.set("n", "<leader>cw", toggle_wrap, { desc = "Toggle [W]rap" })
