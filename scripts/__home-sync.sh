2255dacb92013b5dbec6f0b85253a838cdd5b1bd
Janpstrunn <janpstrunn@disroot.org>
2025-02-06 14:44:35 -0300

    refactor!: Use a different rsync option for granular syncing

--- scripts/__home-sync.sh
+++ scripts/__home-sync.sh
@@ -1,27 +1,11 @@
 #!/bin/env bash
 
-function rsync_base() {
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/";
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/";
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/";
-  rsync --progress -avh --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/"
+function rsync_home() {
+	if [ ! -d "$HOME/home" ]; then
+		echo "Please, create a new directory at:"
+		echo "$HOME/home"
+	fi
+	rsync --progress -avh --delete --exclude-from=$HOME/.rsync-ignore "$HOME/" "$HOME/home/"
 }
 
-function rsync_root() {
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-home.txt "$HOME/" "$HOME/home/" && sudo chown -R $USER:$USER "$HOME/";
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-config.txt "$HOME/.config/" "$HOME/home/.config/" && sudo chown -R $USER:$USER "$HOME/.config";
-  rsync --progress -avh --delete --files-from=$SCRIPTS/rsync/rsync-local.txt "$HOME/.local/share/" "$HOME/home/.local/share/" && sudo chown -R $USER:$USER "$HOME/.local/share";
-  rsync --progress -avh --delete --exclude=.config/ "$HOME/dotfiles/" "$HOME/home/dotfiles/" && sudo chown -R $USER:$USER "$HOME/dotfiles"
-}
-
-case "$1" in
-  "base")
-    rsync_base
-    ;;
-  "root")
-    rsync_root
-    ;;
-  "")
-    echo "Options: base, root"
-    ;;
-esac
+rsync_home
