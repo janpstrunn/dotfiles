a7bda471507121ff95d26f0e7461a7ef2220094d
Augusto Coronel <anstrinn@disroot.org>
2025-02-07 09:23:29 -0300

    nvim: Disable obsidian.nvim in Termux, and remove Zettelkasten ID

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -2,7 +2,13 @@ return {
 	"epwalsh/obsidian.nvim",
 	version = "*",
 	lazy = false,
-	ft = "markdown",
+	enabled = function()
+		if vim.cmd("silent! uname -a | grep 'Android' || echo 'false'") == false then
+			return false
+		else
+			return true
+		end
+	end,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
@@ -56,9 +62,29 @@ return {
 			template = "Journal - Daily.md",
 		},
 
-		wiki_link_func = function(opts)
-			return require("obsidian.util").wiki_link_id_prefix(opts)
+		note_id_func = function(title)
+			if title ~= nil then
+				return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				local suffix = ""
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+				return tostring(os.time()) .. "-" .. suffix
+			end
 		end,
+
+		---@param spec { id: string, dir: obsidian.Path, title: string|? }
+		---@return string|obsidian.Path The full path to the new note.
+		note_path_func = function(spec)
+			local path = spec.dir / tostring(spec.id)
+			return path:with_suffix(".md")
+		end,
+
+		wiki_link_func = function(opts)
+			return opts.file
+		end,
+
 		markdown_link_func = function(opts)
 			return require("obsidian.util").markdown_link(opts)
 		end,
@@ -89,16 +115,6 @@ return {
 			},
 		},
 
-		---@param url string
-		follow_url_func = function(url)
-			vim.fn.jobstart({ "open", url }) -- Mac OS
-		end,
-
-		---@param img string
-		follow_img_func = function(img)
-			vim.fn.jobstart({ "qlmanage", "-p", img }) -- Mac OS quick look preview
-		end,
-
 		use_advanced_uri = false,
 
 		open_app_foreground = false,
@@ -116,60 +132,36 @@ return {
 		},
 
 		sort_by = "modified",
-		sort_reversed = false,
+		sort_reversed = true,
 
 		search_max_lines = 1000,
 
 		open_notes_in = "current",
 
-		callbacks = {
-			---@param client obsidian.Client
-			post_setup = function(client) end,
-
-			---@param client obsidian.Client
-			---@param note obsidian.Note
-			enter_note = function(client, note) end,
-
-			---@param client obsidian.Client
-			---@param note obsidian.Note
-			leave_note = function(client, note) end,
-
-			---@param client obsidian.Client
-			---@param note obsidian.Note
-			pre_write_note = function(client, note) end,
-
-			---@param client obsidian.Client
-			---@param workspace obsidian.Workspace
-			post_set_workspace = function(client, workspace) end,
-		},
+		-- callbacks = {
+		-- 	---@param client obsidian.Client
+		-- 	post_setup = function(client) end,
+		--
+		-- 	---@param client obsidian.Client
+		-- 	---@param note obsidian.Note
+		-- 	enter_note = function(client, note) end,
+		--
+		-- 	---@param client obsidian.Client
+		-- 	---@param note obsidian.Note
+		-- 	leave_note = function(client, note) end,
+		--
+		-- 	---@param client obsidian.Client
+		-- 	---@param note obsidian.Note
+		-- 	pre_write_note = function(client, note) end,
+		--
+		-- 	---@param client obsidian.Client
+		-- 	---@param workspace obsidian.Workspace
+		-- 	post_set_workspace = function(client, workspace) end,
+		-- },
 
 		ui = {
-			enable = false, -- set to false to disable all additional syntax features
-			update_debounce = 200, -- update delay after a text change (in milliseconds)
-			max_file_length = 5000, -- disable UI features for files with more than this many lines
-			checkboxes = {
-				[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-				["x"] = { char = "", hl_group = "ObsidianDone" },
-			},
-			bullets = { char = "•", hl_group = "ObsidianBullet" },
-			external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-			reference_text = { hl_group = "ObsidianRefText" },
-			highlight_text = { hl_group = "ObsidianHighlightText" },
-			tags = { hl_group = "ObsidianTag" },
-			block_ids = { hl_group = "ObsidianBlockID" },
-			hl_groups = {
-				ObsidianTodo = { bold = true, fg = "#f78c6c" },
-				ObsidianDone = { bold = true, fg = "#89ddff" },
-				ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-				ObsidianTilde = { bold = true, fg = "#ff5370" },
-				ObsidianImportant = { bold = true, fg = "#d73128" },
-				ObsidianBullet = { bold = true, fg = "#89ddff" },
-				ObsidianRefText = { underline = true, fg = "#c792ea" },
-				ObsidianExtLinkIcon = { fg = "#c792ea" },
-				ObsidianTag = { italic = true, fg = "#7c5cff" },
-				ObsidianBlockID = { italic = true, fg = "#7c5cff" },
-				ObsidianHighlightText = { bg = "#75662e" },
-			},
+			-- Ref: https://github.com/epwalsh/obsidian.nvim
+			enable = false,
 		},
 
 		attachments = {
