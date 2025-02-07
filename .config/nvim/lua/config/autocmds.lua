201d41568dce4f61065ce2d45000eede80ec8188
Janpstrunn <janpstrunn@particular.slmail.me>
2025-02-07 09:24:38 -0300

    nvim: Disable footnote autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -29,29 +29,29 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 -- Organize Footnotes in Markdown Files
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.md",
-	callback = function(args)
-		local mode = vim.api.nvim_get_mode().mode
-		io.output(io.tmpfile())
-		local buf = args.buf or vim.api.nvim_get_current_buf()
-		if vim.bo.modified == true and mode == "n" and vim.bo[buf].filetype ~= "markdown" then
-			vim.cmd("silent! lua require('footnote').organize_footnotes()")
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = "*.md",
+-- 	callback = function(args)
+-- 		local mode = vim.api.nvim_get_mode().mode
+-- 		io.output(io.tmpfile())
+-- 		local buf = args.buf or vim.api.nvim_get_current_buf()
+-- 		if vim.bo.modified == true and mode == "n" and vim.bo[buf].filetype ~= "markdown" then
+-- 			vim.cmd("silent! lua require('footnote').organize_footnotes()")
+-- 		end
+-- 	end,
+-- })
 
 -- Run Conform
 vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
-  pattern = "*",
-  callback = function(args)
-    local buf = args.buf or vim.api.nvim_get_current_buf()
-    if vim.fn.mode() == "n" then
-      vim.defer_fn(function()
-        if vim.api.nvim_buf_is_valid(buf) then
-          require("conform").format({ bufnr = buf })
-        end
-      end, 100)
-    end
-  end,
+	pattern = "*",
+	callback = function(args)
+		local buf = args.buf or vim.api.nvim_get_current_buf()
+		if vim.fn.mode() == "n" then
+			vim.defer_fn(function()
+				if vim.api.nvim_buf_is_valid(buf) then
+					require("conform").format({ bufnr = buf })
+				end
+			end, 100)
+		end
+	end,
 })
