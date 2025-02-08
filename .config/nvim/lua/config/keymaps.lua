6297c405b683c2deda7c95e06ece2ff6570c8cef
Janpstrunn <janpstrunn@disroot.org>
2025-02-08 14:29:25 -0300

    nvim: Add additions bindings for markdown

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,4 @@
 local keymap = vim.keymap
-local M = {}
 
 -- Toggle Spelling
 
@@ -354,7 +353,7 @@ end
 
 vim.keymap.set("n", "<leader>gr", id_handle, { desc = "Copy Block [R]eference" })
 
--- Week Daily
+-- Week
 
 local function open_weeknote()
 	local obsidian_dir = os.getenv("JOURNAL") .. "/Week/"
@@ -371,9 +370,70 @@ local function open_weeknote()
 	vim.cmd("edit " .. weeknote_path)
 end
 
-vim.keymap.set("n", "<leader>dw", open_weeknote, { desc = "Open [W]eek Note" })
+vim.keymap.set("n", "<leader>dw", open_weeknote, { desc = "[W]eek Note" })
 
--- Week Monthly
+-- Week Notes Review
+
+local function open_weekly_notes()
+	local obsidian_dir = os.getenv("JOURNAL") .. "/Daily/"
+	local current_file = vim.fn.expand("%:t")
+	local year, week
+
+	local match_year, match_week = current_file:match("(%d+)-W(%d+)%.md")
+	if match_year and match_week then
+		year, week = tonumber(match_year), tonumber(match_week)
+	else
+		year = tonumber(os.date("%Y"))
+		week = tonumber(os.date("%U"))
+	end
+
+	local first_day_of_year = os.time({ year = year, month = 1, day = 1 })
+	local first_weekday = os.date("*t", first_day_of_year).wday -- Lua's wday: Sunday = 1
+	local days_to_first_sunday = (first_weekday == 1) and 0 or (8 - first_weekday)
+	local first_sunday = first_day_of_year + days_to_first_sunday * 86400
+	local week_start = first_sunday + (week - 1) * 7 * 86400
+
+	for i = 0, 6 do
+		local day = os.date("%Y-%m-%d", week_start + i * 86400)
+		local note_path = obsidian_dir .. day .. ".md"
+		vim.cmd("silent edit " .. note_path)
+	end
+end
+
+vim.keymap.set("n", "<leader>dr", open_weekly_notes, { desc = "[R]eview Week Notes" })
+
+-- Insert Week Notes
+
+local function insert_weekly_notes()
+	local current_file = vim.fn.expand("%:t")
+	local year, week
+
+	local match_year, match_week = current_file:match("(%d+)-W(%d+)%.md")
+	if match_year and match_week then
+		year, week = tonumber(match_year), tonumber(match_week)
+	else
+		year = tonumber(os.date("%Y"))
+		week = tonumber(os.date("%U"))
+	end
+
+	local first_day_of_year = os.time({ year = year, month = 1, day = 1 })
+	local first_weekday = os.date("*t", first_day_of_year).wday
+	local days_to_first_sunday = (first_weekday == 1) and 0 or (8 - first_weekday)
+	local first_sunday = first_day_of_year + days_to_first_sunday * 86400
+	local week_start = first_sunday + (week - 1) * 7 * 86400
+
+	local lines = {}
+	for i = 0, 6 do
+		local day = os.date("%Y-%m-%d", week_start + i * 86400)
+		table.insert(lines, string.format("[[%s]]", day))
+	end
+
+	vim.api.nvim_put(lines, "l", true, true)
+end
+
+vim.keymap.set("n", "<leader>di", insert_weekly_notes, { desc = "Insert all daily notes for this week" })
+
+-- Monthly
 
 local function open_monthnote()
 	local obsidian_dir = os.getenv("JOURNAL") .. "/Month/"
@@ -387,9 +447,9 @@ local function open_monthnote()
 	vim.cmd("edit " .. monthnote_path)
 end
 
-vim.keymap.set("n", "<leader>dm", open_monthnote, { desc = "Open [M]onth Note" })
+vim.keymap.set("n", "<leader>dm", open_monthnote, { desc = "[M]onth Note" })
 
--- Week Yearly
+-- Yearly
 
 local function open_yearnote()
 	local obsidian_dir = os.getenv("JOURNAL")
@@ -401,7 +461,7 @@ local function open_yearnote()
 	vim.cmd("edit " .. yearnote_path)
 end
 
-vim.keymap.set("n", "<leader>dy", open_yearnote, { desc = "Open [Y]ear Note" })
+vim.keymap.set("n", "<leader>dy", open_yearnote, { desc = "[Y]ear Note" })
 
 -- Markdown Formatter
 -- Rules
