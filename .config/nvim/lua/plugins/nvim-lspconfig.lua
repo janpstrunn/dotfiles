8912c6e263b0e1e13f0d19f20ce97bf13e5bdf9a
Janpstrunn <janpstrunn@disroot.org>
2025-02-10 13:16:56 -0300

    nvim!: Refactor whole config to use LazyVim distro

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -9,205 +9,187 @@
 -- "stylua",
 
 local config = function()
-	require("neoconf").setup({})
-	local cmp_nvim_lsp = require("cmp_nvim_lsp")
-	local lspconfig = require("lspconfig")
-	local capabilities = cmp_nvim_lsp.default_capabilities()
+  local lspconfig = require("lspconfig")
 
-	-- lua
-	lspconfig.lua_ls.setup({
-		capabilities = capabilities,
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { "vim" },
-				},
-				workspace = {
-					library = {
-						vim.fn.expand("$VIMRUNTIME/lua"),
-						vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
-					},
-				},
-			},
-		},
-	})
+  -- lua
+  lspconfig.lua_ls.setup({
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          library = {
+            vim.fn.expand("$VIMRUNTIME/lua"),
+            vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
+          },
+        },
+      },
+    },
+  })
 
-	-- json
-	lspconfig.jsonls.setup({
-		capabilities = capabilities,
-		filetypes = { "json", "jsonc" },
-		init_options = {
-			provideFormatter = true,
-		},
-	})
+  -- json
+  lspconfig.jsonls.setup({
+    filetypes = { "json", "jsonc" },
+    init_options = {
+      provideFormatter = true,
+    },
+  })
 
-	-- python
-	lspconfig.pyright.setup({
-		capabilities = capabilities,
-		settings = {
-			pyright = {
-				disableOrganizeImports = false,
-				analysis = {
-					useLibraryCodeForTypes = true,
-					autoSearchPaths = true,
-					diagnosticMode = "workspace",
-					autoImportCompletions = true,
-				},
-			},
-		},
-	})
+  -- python
+  lspconfig.pyright.setup({
+    settings = {
+      pyright = {
+        disableOrganizeImports = false,
+        analysis = {
+          useLibraryCodeForTypes = true,
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          autoImportCompletions = true,
+        },
+      },
+    },
+  })
 
-	-- typescript
-	lspconfig.ts_ls.setup({
-		capabilities = capabilities,
-		filetypes = {
-			"typescript",
-			"javascript",
-			"typescriptreact",
-			"javascriptreact",
-		},
-		commands = {},
-		settings = {
-			typescript = {
-				indentStyle = "space",
-				indentSize = 2,
-			},
-		},
-	})
+  -- typescript
+  lspconfig.ts_ls.setup({
+    filetypes = {
+      "typescript",
+      "javascript",
+      "typescriptreact",
+      "javascriptreact",
+    },
+    commands = {},
+    settings = {
+      typescript = {
+        indentStyle = "space",
+        indentSize = 2,
+      },
+    },
+  })
 
-	-- bash
-	lspconfig.bashls.setup({
-		capabilities = capabilities,
-		filetypes = { "sh", "aliasrc" },
-		settings = {
-			bashIde = {
-				globPattern = "*@(.sh|.inc|.bash|.command)",
-			},
-		},
-	})
+  -- bash
+  lspconfig.bashls.setup({
+    filetypes = { "sh", "aliasrc" },
+    settings = {
+      bashIde = {
+        globPattern = "*@(.sh|.inc|.bash|.command)",
+      },
+    },
+  })
 
-	-- markdown
-	require("lspconfig").marksman.setup({
-		capabilities = capabilities,
-		filetypes = { "markdown", "markdown.mdx" },
-	})
-	require("lspconfig").markdown_oxide.setup({
-		capabilities = capabilities,
-		filetypes = { "markdown" },
-	})
+  -- markdown
+  require("lspconfig").marksman.setup({
+    filetypes = { "markdown", "markdown.mdx" },
+  })
+  require("lspconfig").markdown_oxide.setup({
+    filetypes = { "markdown" },
+  })
 
-	-- rust
-	require("lspconfig").bacon_ls.setup({
-		capabilities = capabilities,
-		filetypes = { "rust" },
-	})
+  -- rust
+  require("lspconfig").bacon_ls.setup({
+    filetypes = { "rust" },
+  })
 
-	-- typescriptreact, javascriptreact, css, sass, scss, less, svelte, vue
-	lspconfig.emmet_ls.setup({
-		capabilities = capabilities,
-		filetypes = {
-			"typescriptreact",
-			"javascriptreact",
-			"javascript",
-			"css",
-			"sass",
-			"scss",
-			"less",
-			"svelte",
-			"vue",
-			"html",
-		},
-	})
+  -- typescriptreact, javascriptreact, css, sass, scss, less, svelte, vue
+  lspconfig.emmet_ls.setup({
+    filetypes = {
+      "typescriptreact",
+      "javascriptreact",
+      "javascript",
+      "css",
+      "sass",
+      "scss",
+      "less",
+      "svelte",
+      "vue",
+      "html",
+    },
+  })
 
-	-- docker
-	lspconfig.dockerls.setup({
-		capabilities = capabilities,
-	})
+  -- docker
+  lspconfig.dockerls.setup({})
 
-	-- C/C++
-	lspconfig.clangd.setup({
-		capabilities = capabilities,
-		cmd = {
-			"clangd",
-			"--offset-encoding=utf-16",
-		},
-	})
+  -- C/C++
+  lspconfig.clangd.setup({
+    cmd = {
+      "clangd",
+      "--offset-encoding=utf-16",
+    },
+  })
 
-	local prettier_d = require("efmls-configs.formatters.prettier_d")
-	local luacheck = require("efmls-configs.linters.luacheck")
-	local stylua = require("efmls-configs.formatters.stylua")
-	local flake8 = require("efmls-configs.linters.flake8")
-	local black = require("efmls-configs.formatters.black")
-	local eslint = require("efmls-configs.linters.eslint")
-	local fixjson = require("efmls-configs.formatters.fixjson")
-	local shellcheck = require("efmls-configs.linters.shellcheck")
-	local shfmt = require("efmls-configs.formatters.shfmt")
-	local hadolint = require("efmls-configs.linters.hadolint")
-	local cpplint = require("efmls-configs.linters.cpplint")
-	local clangformat = require("efmls-configs.formatters.clang_format")
+  local prettier_d = require("efmls-configs.formatters.prettier_d")
+  local luacheck = require("efmls-configs.linters.luacheck")
+  local stylua = require("efmls-configs.formatters.stylua")
+  local flake8 = require("efmls-configs.linters.flake8")
+  local black = require("efmls-configs.formatters.black")
+  local eslint = require("efmls-configs.linters.eslint")
+  local fixjson = require("efmls-configs.formatters.fixjson")
+  local shellcheck = require("efmls-configs.linters.shellcheck")
+  local shfmt = require("efmls-configs.formatters.shfmt")
+  local hadolint = require("efmls-configs.linters.hadolint")
+  local cpplint = require("efmls-configs.linters.cpplint")
+  local clangformat = require("efmls-configs.formatters.clang_format")
 
-	-- configure efm server
-	lspconfig.efm.setup({
-		filetypes = {
-			"lua",
-			"python",
-			"json",
-			"jsonc",
-			"sh",
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"svelte",
-			"vue",
-			"docker",
-			"html",
-			"css",
-			"c",
-			"cpp",
-		},
-		init_options = {
-			documentFormatting = true,
-			documentRangeFormatting = true,
-			hover = true,
-			documentSymbol = true,
-			codeAction = true,
-			completion = true,
-		},
-		settings = {
-			languages = {
-				lua = { luacheck, stylua },
-				python = { flake8, black },
-				typescript = { eslint, prettier_d },
-				json = { eslint, fixjson },
-				jsonc = { eslint, fixjson },
-				sh = { shellcheck, shfmt },
-				javascript = { eslint, prettier_d },
-				javascriptreact = { eslint, prettier_d },
-				typescriptreact = { eslint, prettier_d },
-				svelte = { eslint, prettier_d },
-				vue = { eslint, prettier_d },
-				docker = { hadolint, prettier_d },
-				html = { prettier_d },
-				css = { prettier_d },
-				c = { clangformat, cpplint },
-				cpp = { clangformat, cpplint },
-			},
-		},
-	})
+  -- configure efm server
+  lspconfig.efm.setup({
+    filetypes = {
+      "lua",
+      "python",
+      "json",
+      "jsonc",
+      "sh",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "svelte",
+      "vue",
+      "docker",
+      "html",
+      "css",
+      "c",
+      "cpp",
+    },
+    init_options = {
+      documentFormatting = true,
+      documentRangeFormatting = true,
+      hover = true,
+      documentSymbol = true,
+      codeAction = true,
+      completion = true,
+    },
+    settings = {
+      languages = {
+        lua = { luacheck, stylua },
+        python = { flake8, black },
+        typescript = { eslint, prettier_d },
+        json = { eslint, fixjson },
+        jsonc = { eslint, fixjson },
+        sh = { shellcheck, shfmt },
+        javascript = { eslint, prettier_d },
+        javascriptreact = { eslint, prettier_d },
+        typescriptreact = { eslint, prettier_d },
+        svelte = { eslint, prettier_d },
+        vue = { eslint, prettier_d },
+        docker = { hadolint, prettier_d },
+        html = { prettier_d },
+        css = { prettier_d },
+        c = { clangformat, cpplint },
+        cpp = { clangformat, cpplint },
+      },
+    },
+  })
 end
 
 return {
-	"neovim/nvim-lspconfig",
-	config = config,
-	lazy = false,
-	dependencies = {
-		"windwp/nvim-autopairs",
-		"williamboman/mason.nvim",
-		"creativenull/efmls-configs-nvim",
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	opts = {},
+  "neovim/nvim-lspconfig",
+  config = config,
+  event = "BufReadPre",
+  lazy = true,
+  dependencies = {
+    "williamboman/mason.nvim",
+    "creativenull/efmls-configs-nvim",
+  },
+  opts = {},
 }
