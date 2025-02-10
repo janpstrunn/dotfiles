3918506c627a350755e800e80140d898808e4a0f
Augusto Coronel <anstrinn@disroot.org>
2025-02-09 21:01:43 -0300

    nvim: Update render-markdown to fix rendering issues

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -172,20 +172,20 @@ local config = function()
 			render_modes = false,
 			-- Replaces '-'|'+'|'*' of 'list_item'
 			-- How deeply nested the list is determines the 'level', how far down at that level determines the 'index'
-			-- If a function is provided both of these values are passed in using 1 based indexing
+			-- If a function is provided both of these values are provided in the context using 1 based indexing
 			-- If a list is provided we index into it using a cycle based on the level
 			-- If the value at that level is also a list we further index into it using a clamp based on the index
 			-- If the item is a 'checkbox' a conceal is used to hide the bullet instead
 			icons = { "", "", "", "" },
 			-- Replaces 'n.'|'n)' of 'list_item'
 			-- How deeply nested the list is determines the 'level', how far down at that level determines the 'index'
-			-- If a function is provided both of these values are passed in using 1 based indexing
+			-- If a function is provided both of these values are provided in the context using 1 based indexing
 			-- If a list is provided we index into it using a cycle based on the level
 			-- If the value at that level is also a list we further index into it using a clamp based on the index
-			ordered_icons = function(level, index, value)
-				value = vim.trim(value)
-				local value_index = tonumber(value:sub(1, #value - 1))
-				return string.format("%d.", value_index > 1 and value_index or index)
+			ordered_icons = function(ctx)
+				local value = vim.trim(ctx.value)
+				local index = tonumber(value:sub(1, #value - 1))
+				return string.format("%d.", index > 1 and index or ctx.index)
 			end,
 			-- Padding to add to the left of bullet point
 			left_pad = 0,
@@ -399,7 +399,9 @@ end
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	enabled = true,
 	ft = { "markdown" },
+	event = "BufReadPost",
 	opts = {},
 	config = config,
 }
