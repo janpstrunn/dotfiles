c031854a90ef28a0568a82f9adf0b4abc742fd76
Janpstrunn <janpstrunn@particular.slmail.me>
2025-02-09 21:02:48 -0300

    nvim: Disable LSP autoformat

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -17,16 +17,16 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 -- Format file on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		local mode = vim.api.nvim_get_mode().mode
-		local filetype = vim.bo.filetype
-		if vim.bo.modified == true and mode == "n" and filetype ~= "oil" then
-			vim.cmd("lua vim.lsp.buf.format()")
-		else
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	callback = function()
+-- 		local mode = vim.api.nvim_get_mode().mode
+-- 		local filetype = vim.bo.filetype
+-- 		if vim.bo.modified == true and mode == "n" and filetype ~= "oil" then
+-- 			vim.cmd("lua vim.lsp.buf.format()")
+-- 		else
+-- 		end
+-- 	end,
+-- })
 
 -- Organize Footnotes in Markdown Files
 -- vim.api.nvim_create_autocmd("BufWritePre", {
