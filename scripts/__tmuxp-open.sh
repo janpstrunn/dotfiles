e83a44803392d8eb8d3f253f59f37c76bf40cafa
Janpstrunn <janpstrunn@disroot.org>
2025-02-11 15:55:27 -0300

    chore: Remove hardcode

--- scripts/__tmuxp-open.sh
+++ scripts/__tmuxp-open.sh
@@ -1,14 +1,18 @@
 #!/bin/env bash
 
+if [ -z "$TERMCMD" ]; then
+  eval $(awk -F= '/^(TERMCMD|EDITOR)=/ {print "export " $1 "=" "\"" $2 "\""}' ~/.env)
+fi
+
 function tp() {
   session=$(find "$HOME/.config/tmuxp/" -type f -name '*.yaml' -printf '%P\n' | awk -F. '{print $1}')
   select=$(echo "$session" | rofi -dmenu)
   if [[ -n "$select" ]]; then
     if tmux has-session -t "$select"; then
-      ghostty -e tmux attach-session -t "$select"
+      $TERMCMD -e tmux attach-session -t "$select"
     else
       tmuxp load "$select" -d
-     ghostty -e tmux attach-session -t "$select"
+      $TERMCMD -e tmux attach-session -t "$select"
     fi
   fi
 }
