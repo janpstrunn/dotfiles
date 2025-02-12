ab4f3cf378938be65a56de2c157fe4f5503ac899
Augusto Coronel <anstrinn@disroot.org>
2025-02-12 10:09:30 -0300

    install: Add more packages, run LSP and add a tip message

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ bluetooth="blueberry gnome-bluetooth-3.0"
 xorg="flameshot sxhkd redshift xwallpaper xclip nsxiv"
 wayland="waybar hyprland hyprlang hyprsunset hyprlock hypridle hyprctl cliphist jq xdg-desktop-portal-hyprland cmake meson cpio pkg-config hyprpolkitagent swww hyprpicker wlogout wl-clipboard wf-recorder"
 
-essentials="gvfs mtpfs gvfs-mtp polkit udiskie udisks2 fuse2 unzip zip direnv atomicparsley zsh ueberzugpp gnome-keyring playerctl ydotool brightnessctl"
+essentials="gvfs mtpfs gvfs-mtp polkit udiskie udisks2 fuse2 unzip zip direnv atomicparsley zsh ueberzugpp gnome-keyring playerctl ydotool brightnessctl at pacman-contrib"
 
 cli="rsync pass tmux pass-otp gocryptfs ffmpeg mpv cmus onefetch fastfetch lynx ranger bpytop eza fd fzf github-cli tldr man lazygit task taskwarrior-tui zoxide ripgrep bat cmatrix figlet tmuxp cmus gitleaks moreutils navi imagemagick just neomutt syncthing neovim mupdf mupdf-tools yt-dlp newsboat ddgr odt2text"
 graphical="seahorse obsidian libreoffice-fresh shotcut gnome-disk-utility obs-studio gimp dmenu dunst neovide kitty foot picom scrcpy rofi-calc rofi networkmanager-openvpn fuzzel swayimg grim slurp satty pavucontrol"
@@ -46,19 +46,19 @@ GPG_KEY=$1
 echo "→ setting up git"
 
 if [ -z "$NAME" ]; then
-	read -r -p "Please enter your git user.name" NAME
-	NAME=${NAME:-"Janpstrunn"}
+  read -r -p "Please enter your git user.name" NAME
+  NAME=${NAME:-"Janpstrunn"}
 fi
 
 if [ -z "$EMAIL" ]; then
-	read -r -p "Please enter your git user.email" EMAIL
-	EMAIL=${EMAIL:-"janpstrunn@particular.slmail.me"}
+  read -r -p "Please enter your git user.email" EMAIL
+  EMAIL=${EMAIL:-"janpstrunn@particular.slmail.me"}
 fi
 
 if [ -z "$GPG_KEY" ]; then
-	git config --global user.signingkey "$GPG_KEY"
-	git config --global commit.gpgsign true
-	git config --global core.excludesFile "$HOME"/.gitignore
+  git config --global user.signingkey "$GPG_KEY"
+  git config --global commit.gpgsign true
+  git config --global core.excludesFile "$HOME"/.gitignore
 fi
 
 git config --global user.name "$NAME"
@@ -87,6 +87,7 @@ sudo systemctl enable syncthing@$USER.service
 echo "→ install fonts"
 
 sudo pacman -S noto-fonts-emoji gnu-free-fonts ttf-ubuntu-nerd ttf-dejavu-nerd ttf-jetbrains-mono-nerd
+yay -S ttf-gabarito-git
 
 # Microsoft Fonts
 # Once it's installed the fonts are saved to /usr/fonts and can be exported,
@@ -111,3 +112,4 @@ echo "You may now reboot to apply some packages and fonts"
 echo "→ How to apply final configurations"
 
 echo "Run: just"
+echo "Tip: Configure /etc/pacman.conf"
