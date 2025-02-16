4f9dab426826d16dd4a0aac217c4cfbe6a226b9c
Augusto Coronel <anstrinn@disroot.org>
2025-02-16 09:27:30 -0300

    nvim: Disable pane-resizer
    LazyVim has an autocmd that does it by default.

--- .config/nvim/lua/plugins/pane-resizer.lua
+++ .config/nvim/lua/plugins/pane-resizer.lua
@@ -1,8 +1,9 @@
 return {
   {
     "mboyov/pane-resizer.nvim",
+    enabled = false,
     config = function()
-      require('pane_resizer').setup({
+      require("pane_resizer").setup({
         NVIMTREE_WIDTH = 40,
         FOCUSED_WIDTH_PERCENTAGE = 0.5,
       })
