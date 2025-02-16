59913a8565a5d9d05f01617f87403b0d2ada5cfa
Janpstrunn <janpstrunn@disroot.org>
2025-02-16 14:47:20 -0300

    chore: Remove unused variables, and change display-popup to splitw

--- scripts/__tmux-task.sh
+++ scripts/__tmux-task.sh
@@ -11,8 +11,8 @@ if [[ "$current_command" == "nvim" ]]; then
   file_name=$(echo "$file_name" | sed 's/\.md//; s/th//')
 
   if [[ -n "$file_name" ]]; then
-    tmux display-popup -h 90% -w 90% -E \
-      "$SHELL" -c "$SCRIPTS/__tskd.sh \"$file_name\" tmux"
+    tmux splitw -v \
+      "$SHELL" -c "$SCRIPTS/__tskd.sh \"$file_name\" tmux && exec $SHELL"
   else
     echo "Could not retrieve the file name."
   fi
