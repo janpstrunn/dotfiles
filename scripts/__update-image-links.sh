b38c5fb23731f9b4af4dea462def8a354b4c142e
Augusto Coronel <anstrinn@disroot.org>
2025-02-21 12:41:40 -0300

    feat: Add verbose

--- scripts/__update-image-links.sh
+++ scripts/__update-image-links.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 
 find . -name '*.md' -exec grep -lE '\.jpg|\.png|\.webp' {} \; | while read -r file; do
-    sed -i -E 's/\.jpg|\.png|\.webp/.avif/g' "$file"
+  sed -i -E 's/\.jpg|\.png|\.webp/.avif/g' "$file"
+  echo "$file" updated
 done
