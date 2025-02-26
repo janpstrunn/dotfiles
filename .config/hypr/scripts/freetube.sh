72c6daf83ef9e423b4b103756a5b2e14b171540d
Augusto Coronel <anstrinn@disroot.org>
2025-02-26 12:09:27 -0300

    hypr, invidious(refactor!): Change how the instance is launched, and rotate tokens per session

--- .config/hypr/scripts/freetube.sh
+++ .config/hypr/scripts/freetube.sh
@@ -1,10 +1,17 @@
 #!/bin/env bash
 
+invidious_config=$HOME/invidious/
+
 function startup() {
-  if tmux has-session -t "invidious"; then
+  if podman ps | grep -q "invidious"; then
     echo "Invidious Instance is already running."
   else
-    tmuxp load "invidious" -d
+    cd "$invidious_config" || {
+      notify-send -u critical "Invidious" "An error occurred"
+      exit 1
+    }
+    sh "$HOME/scripts/__invidious-token-generator.sh"
+    podman-compose up -d
     echo "Invidious Instance launched."
     sleep 5
   fi
---
+++ scripts/__invidious-token-generator.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+invidious_config=$HOME/invidious/docker-compose.yml
+
+docker run quay.io/invidious/youtube-trusted-session-generator >token.txt
+
+visitor_data=$(grep "visitor_data" token.txt | awk -F ':' '{print $2}')
+po_token=$(grep "po_token" token.txt | awk -F ':' '{print $2}')
+
+sed -i "s/^\(\s*visitor_data:\s*\).*/\1$visitor_data/" "$invidious_config" && visitor_ok=True
+sed -i "s/^\(\s*po_token:\s*\).*/\1$po_token/" "$invidious_config" && po_ok=True
+
+sed -i "s/^\([[:space:]]*visitor_data:\)[[:space:]]*.*/\1 $visitor_data/" "$invidious_config" && visitor_ok=True
+sed -i "s/^\([[:space:]]*po_token:\)[[:space:]]*.*/\1 $po_token/" "$invidious_config" && po_ok=True
+
+if [ "$visitor_ok" == "True" ] || [ "$po_ok" == "True" ]; then
+  echo "Tokens successfully updated!"
+else
+  echo "Tokens failed to be updated!"
+fi
