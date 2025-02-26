0a823397469ad1d4869dc866ca56ca85f11540bc
Janpstrunn <janpstrunn@disroot.org>
2025-02-26 14:41:33 -0300

    hypr(feat): Run invidious token generator if not ran today

--- .config/hypr/scripts/freetube.sh
+++ .config/hypr/scripts/freetube.sh
@@ -1,6 +1,19 @@
 #!/bin/env bash
 
-invidious_config=$HOME/invidious/
+invidious_config="$HOME/invidious/"
+invidious_last_run="$HOME/.cache/invidious_last_run"
+
+function token_generator() {
+  local today
+  today=$(date +%Y-%m-%d)
+
+  if [[ -f "$invidious_last_run" ]] && grep -q "$today" "$invidious_last_run"; then
+    return 1
+  fi
+
+  echo "$today" >"$invidious_last_run"
+  return 0
+}
 
 function startup() {
   if podman ps | grep -q "invidious"; then
@@ -10,7 +23,11 @@ function startup() {
       notify-send -u critical "Invidious" "An error occurred"
       exit 1
     }
-    sh "$HOME/scripts/__invidious-token-generator.sh"
+
+    if token_generator; then
+      sh "$HOME/scripts/__invidious-token-generator.sh"
+    fi
+
     podman-compose up -d
     echo "Invidious Instance launched."
     sleep 5
