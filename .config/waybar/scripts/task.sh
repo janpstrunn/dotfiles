f1a6dcf17de8f3094e8edb81b197820080c6ef53
Augusto Coronel <anstrinn@disroot.org>
2025-03-01 18:39:29 -0300

    waybar(feat): Change task type shown on click
    Left and Right Mouse Buttons change what kind of tasks are shown in the
    Waybar. Left for all, right for today.

--- .config/waybar/scripts/task.sh
+++ .config/waybar/scripts/task.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-tasks=$(task status:pending count)
+modefile=$HOME/.cache/waybar-task
+
+case "$1" in
+"all") echo "all" >"$modefile" ;;
+"today") echo "today" >"$modefile" ;;
+esac
+
+if [ -f "$modefile" ]; then
+  if [ "$(cat "$modefile")" = "all" ]; then
+    tasks=$(task status:pending count)
+  elif [ "$(cat "$modefile")" = "today" ]; then
+    tasks=$(task status:pending due:today count)
+  fi
+else
+  tasks=$(task status:pending count)
+fi
+
+tooltip="LMB: Show all remaining tasks"
+tooltip+="\nRMB: Show today remaining tasks"
 
 if [ -z "$tasks" ]; then
-	echo '{"text": "No tasks", "class": "no-tasks"}'
+  echo "{\"text\": \"No tasks\", \"tooltip\": \"$tooltip\"}"
 else
-	echo "{\"text\":\"$tasks tasks\", \"class\":\"taskwarrior\"}"
+  echo "{\"text\":\"$tasks tasks\", \"tooltip\": \"$tooltip\"}"
 fi
