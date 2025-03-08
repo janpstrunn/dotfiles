d29bf80b576932b7a5447b856c8c81384399035e
Augusto Coronel <anstrinn@disroot.org>
2025-03-08 09:21:19 -0300

    scripts: Add flashcard script
    Feature git commits when finish flashcard review.

---
+++ scripts/__flashcards.sh
@@ -0,0 +1,33 @@
+#!/bin/env bash
+
+FLASH_DIR="$OBSIDIAN/test"
+FLASH_FILE="$FLASH_DIR/flashcards.md"
+NOW=$(date +%F)
+
+if [ ! -f "$FLASH_FILE" ]; then
+  echo "Flashcard not found!"
+  sleep 1
+  return 1
+fi
+
+function flash_state() {
+  du --bytes "$FLASH_FILE" | awk '{print $1}'
+}
+
+function commit() {
+  git -C "$FLASH_DIR" add flashcards.md &&
+    git -C "$FLASH_DIR" commit flashcards.md -m "flashcard: update $NOW"
+  return 0
+}
+
+function flash() {
+  INITIAL_STATE=$(flash_state)
+  mdfc -o -w 100 "$FLASH_FILE"
+  CURRENT_STATE=$(flash_state)
+
+  if [ "$CURRENT_STATE" -ne "$INITIAL_STATE" ]; then
+    commit
+  fi
+}
+
+flash
