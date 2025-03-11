37ebb05d61da5d3c5164f63c09b8e704714c8b7a
Janpstrunn <janpstrunn@disroot.org>
2025-03-11 13:15:46 -0300

    refactor: New convention to source environment variables, add lib scripts for deduplication, and add .localenv template

--- scripts/__tmuxp-open.sh
+++ scripts/__tmuxp-open.sh
@@ -2,13 +2,14 @@
 
 # https://github.com/janpstrunn/dotfiles/blob/main/scripts/__tmuxp-open.sh
 
-if [ -z "$TERMCMD" ]; then
-  var=$(grep "TERMCMD" "$HOME/.localenv")
-  export $var
-fi
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+source "$SCRIPT_DIR/lib/get_var.sh"
 
 function tp() {
   session=$(find "$HOME/.config/tmuxp/" -type f -name '*.yaml' -printf '%P\n' | awk -F. '{print $1}')
+  if [ -z "$session" ]; then
+    echo "Failed to get tmuxp files at $HOME/.config/tmuxp"
+  fi
   select=$(echo "$session" | rofi -dmenu)
   if [[ -n "$select" ]]; then
     if tmux has-session -t "$select"; then
@@ -20,4 +21,5 @@ function tp() {
   fi
 }
 
+get_term # Get TERMCMD env
 tp
