318b3643c0b3dcfff7c6792730f2a7bb7e57b436
Augusto Coronel <anstrinn@disroot.org>
2025-03-11 15:52:34 -0300

    refactor!: Use fzf or rofi to select an album, and use get_env lib

--- scripts/__get-album.sh
+++ scripts/__get-album.sh
@@ -2,14 +2,58 @@
 
 # https://github.com/janpstrunn/dotfiles/blob/main/scripts/__get-album.sh
 
-if [ -z "$WALLPAPERS" ]; then
-  var=$(grep "WALLPAPERS" "$HOME/.localenv")
-  export $var
+if ! command -v swayimg &>/dev/null; then
+  echo "swayimg could not be found. Please install it."
+  exit 1
 fi
 
-function get_wallpaper() {
-  export album=$(ls "$WALLPAPERS" | fzf --prompt "Select an album")
-  swayimg --gallery "$WALLPAPERS/$album"
-}
+SCRIPT_DIR="$(dirname "$(realpath "")")"
+source "$SCRIPT_DIR/lib/get_env.sh"
 
 get_wallpaper
+
+if [ -z "$WALLPAPERS" ]; then
+  echo "Error: WALLPAPERS env at .localenv not found"
+  exit 1
+fi
+
+function _fzf() {
+  if ! command -v fzf &>/dev/null; then
+    echo "fzf could not be found. Please install it."
+    exit 1
+  fi
+  fzf --prompt 'Select an album: '
+}
+
+function _rofi() {
+  if ! command -v rofi &>/dev/null; then
+    echo "rofi could not be found. Please install it."
+    exit 1
+  fi
+  rofi -dmenu -i -no-levenshtein-sort -width 1000 -p '> ' -mesg "<span color='#7c5cff'>Select an album</span>" "$@"
+}
+
+function main() {
+  local run
+  if [ "$mode" == "fzf" ]; then
+    run=_fzf
+  elif [ "$mode" == "rofi" ]; then
+    run=_rofi
+  fi
+  export album=$(ls "$WALLPAPERS" | "${run}") && swayimg --gallery "$WALLPAPERS/$album"
+}
+
+case "$1" in
+"rofi")
+  mode=rofi
+  main
+  ;;
+"fzf")
+  mode=fzf
+  main
+  ;;
+*)
+  mode=fzf
+  main
+  ;;
+esac
