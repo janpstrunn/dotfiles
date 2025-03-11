ddf66c9fbf34267e1adf7a8e7c6cd55e3a0b7974
Janpstrunn <janpstrunn@disroot.org>
2025-03-11 14:54:21 -0300

    chore: Add version check for dunst

--- scripts/__clearboard.sh
+++ scripts/__clearboard.sh
@@ -2,14 +2,21 @@
 
 # https://github.com/janpstrunn/dotfiles/blob/main/scripts/__clearboard.sh
 
-clipmethod=$(echo "$XDG_SESSION_TYPE")
+if ! command -v dunst &>/dev/null; then
+  echo "dunst could not be found. Please install it."
+  exit 1
+fi
+
+clipmethod="$XDG_SESSION_TYPE"
 
 function clearboard() {
   if [ "$clipmethod" = "x11" ]; then
     dunstctl close-all
     echo "" | xclip -sel clip
   elif [ "$clipmethod" = "wayland" ]; then
-    cliphist wipe
+    if command -v cliphist &>/dev/null; then
+      cliphist wipe
+    fi
     dunstctl close-all
     echo "" | wl-copy
   fi
