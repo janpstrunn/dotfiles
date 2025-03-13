aae70b27eb4ca1a555ffeeabf5181607f0ce7b0e
Augusto Coronel <anstrinn@disroot.org>
2025-03-13 09:15:54 -0300

    feat: Improve notification messages, and add tmux functions

--- scripts/lib/tmux_functions.sh
+++ scripts/lib/tmux_functions.sh
@@ -24,9 +24,29 @@ function close_tmux() {
   local select="$1"
 
   if tmux has-session -t "$select"; then
-    tmux kill-session -t "$select" && notify-send -u low "Session $select has been closed"
+    tmux kill-session -t "$select" && notify-send -u low "Tmux: Success" "Session $select has been closed"
   else
     echo "Session $select isn't running"
-    notify-send -u normal "Session $select isn't running"
+    notify-send -u normal "Tmux: Error" "Session $select isn't running"
+  fi
+}
+
+function open_tmux() {
+  local select="$1"
+
+  if tmux has-session -t "$select" 2>/dev/null; then
+    tmux attach-session -t "$select"
+  else
+    notify-send -u low "Tmux: Error" "No session found"
+  fi
+}
+
+function open_tmux_term() {
+  local select="$1"
+
+  if tmux has-session -t "$select" 2>/dev/null; then
+    exec "$TERMCMD" -e tmux attach-session -t "$select"
+  else
+    notify-send -u low "Tmux: Error" "No session found"
   fi
 }
