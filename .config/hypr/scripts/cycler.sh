991cffd00ca49d3d54009ac8c430dee7ccaf692b
Janpstrunn <janpstrunn@disroot.org>
2025-03-23 10:44:40 -0300

    hypr: Add cycler script
    - Switch between different instances of the same application

---
+++ .config/hypr/scripts/cycler.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+active_class=$(hyprctl activewindow -j | jq -r '.class')
+
+mapfile -t windows < <(hyprctl clients -j | jq -r ".[] | select(.class == \"$active_class\") | .address")
+
+current_window=$(hyprctl activewindow -j | jq -r '.address')
+echo "Current window: $current_window"
+
+for i in "${!windows[@]}"; do
+  if [[ "${windows[i]}" == "$current_window" ]]; then
+    next_index=$(((i + 1) % ${#windows[@]}))
+    next_window=${windows[next_index]}
+    hyprctl dispatch focuswindow address:"$next_window"
+    exit
+  fi
+done
