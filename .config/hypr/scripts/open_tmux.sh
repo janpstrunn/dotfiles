378709111085e7594ff6a051be202807d9c2de41
Augusto Coronel <anstrinn@disroot.org>
2025-03-23 17:06:47 -0300

    hypr(chore): Add exception to tmuxp 'task'

--- .config/hypr/scripts/open_tmux.sh
+++ .config/hypr/scripts/open_tmux.sh
@@ -5,6 +5,10 @@ function tpo() {
   local class_name="tmux-$session_name"
   local title_name="$class_name"
 
+  if [ "$session_name" == "task" ]; then
+    class_name="$session_name"
+  fi
+
   if tmux has-session -t "$session_name" 2>/dev/null; then
     if pgrep -f "tmux attach-session -t $session_name" >/dev/null; then
       hyprctl dispatch focuswindow class:"$class_name"
@@ -12,7 +16,7 @@ function tpo() {
       exec kitty --class "$class_name" --title "$title_name" -e tmux attach-session -t "$session_name"
     fi
   else
-    exec kitty --class "$class_name" --title "$title_name" -e tmuxp load "$session_name" -d && tmux attach-session -t "$session_name"
+    exec kitty --class "$class_name" --title "$title_name" -e tmuxp load "$session_name"
   fi
 }
 
