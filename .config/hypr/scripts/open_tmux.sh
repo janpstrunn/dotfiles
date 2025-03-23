7880207373302fe9c7e54ad208460e8fe71bdbf8
Janpstrunn <janpstrunn@disroot.org>
2025-03-23 13:55:52 -0300

    hypr(scripts): If tmux session is running, move to it

--- .config/hypr/scripts/open_tmux.sh
+++ .config/hypr/scripts/open_tmux.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 function tpo() {
-  if tmux has-session -t "$1"; then
-    exec kitty -e tmux attach-session -t "$1"
+  local session_name="$1"
+  local class_name="tmux-$session_name"
+
+  if tmux has-session -t "$session_name" 2>/dev/null; then
+    if pgrep -f "tmux attach-session -t $session_name" >/dev/null; then
+      hyprctl dispatch focuswindow class:"$class_name"
+    else
+      exec kitty --class "$class_name" -e tmux attach-session -t "$session_name"
+    fi
   else
-    exec kitty -e tmuxp load "$1" -d && tmux attach-session -t "$1"
+    exec kitty --class "$class_name" -e tmuxp load "$session_name" -d && tmux attach-session -t "$session_name"
   fi
 }
 
-tpo $1
+tpo "$1"
