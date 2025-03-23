9315945a181e7880ebc859c56a6c0bee56fe0a9c
Augusto Coronel <anstrinn@disroot.org>
2025-03-23 14:39:20 -0300

    hypr(chore): Remove unnecessary code

--- .config/hypr/scripts/switcher.sh
+++ .config/hypr/scripts/switcher.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-select_client() {
-  local clients="$1"
-  local total_clients="$2"
+help_color="#7c5cff"
+label="#f067fc"
 
+delete="Ctrl+q"
+
+total_clients=$(hyprctl clients -j | jq -r ".[].title" | wc -l)
+clients=$(hyprctl clients -j | jq -r '.[] | "\(.title)"')
+
+HELP="<span color='${label}'>Actions: </span><span color='${help_color}'>${delete}</span>: Close"
+
+_rofi() {
   rofi -dmenu -i -no-levenshtein-sort -width 1000 -p "$total_clients" -mesg "${HELP}" -kb-custom-1 "${delete}" <<<"$clients"
 }
 
@@ -17,22 +24,12 @@ close_client() {
   hyprctl dispatch closewindow address:"$client_address" && notify-send -u low "Rofi" "$client_name closed"
 }
 
-help_color="#7c5cff"
-label="#f067fc"
-
-delete="Ctrl+q"
-
-total_clients=$(hyprctl clients -j | jq -r ".[].title" | wc -l)
-clients=$(hyprctl clients -j | jq -r '.[] | "\(.title)"')
-
-HELP="<span color='${label}'>Actions: </span><span color='${help_color}'>${delete}</span>: Close"
-
 if [ "$total_clients" -eq 0 ]; then
   echo "No clients found."
   exit 1
 fi
 
-client_name=$(select_client "$clients" "$total_clients" "$HELP" "$delete")
+client_name=$(_rofi "$clients")
 val=$?
 
 case "$val" in
