fad82ab260333109eb561380b7d70b77f39cbfa4
Augusto Coronel <anstrinn@disroot.org>
2025-03-23 14:22:30 -0300

    hypr(feat): Change tmux title

--- .config/hypr/scripts/open_tmux.sh
+++ .config/hypr/scripts/open_tmux.sh
@@ -3,15 +3,16 @@
 function tpo() {
   local session_name="$1"
   local class_name="tmux-$session_name"
+  local title_name="$class_name"
 
   if tmux has-session -t "$session_name" 2>/dev/null; then
     if pgrep -f "tmux attach-session -t $session_name" >/dev/null; then
       hyprctl dispatch focuswindow class:"$class_name"
     else
-      exec kitty --class "$class_name" -e tmux attach-session -t "$session_name"
+      exec kitty --class "$class_name" --title "$title_name" -e tmux attach-session -t "$session_name"
     fi
   else
-    exec kitty --class "$class_name" -e tmuxp load "$session_name" -d && tmux attach-session -t "$session_name"
+    exec kitty --class "$class_name" --title "$title_name" -e tmuxp load "$session_name" -d && tmux attach-session -t "$session_name"
   fi
 }
 
