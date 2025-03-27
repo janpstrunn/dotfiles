04e9a5f0c70cdefe8bf62dfc976b79f15bc80606
Janpstrunn <janpstrunn@disroot.org>
2025-03-27 11:40:59 -0300

    hypr(scripts): Improve tmux session switching

--- .config/hypr/scripts/open_tmux.sh
+++ .config/hypr/scripts/open_tmux.sh
@@ -2,21 +2,22 @@
 
 function tpo() {
   local session_name="$1"
-  local class_name="tmux-$session_name"
+  local class_name="tmux"
   local title_name="$class_name"
 
-  if [ "$session_name" == "task" ]; then
+  if [[ "$session_name" == "task" ]]; then
     class_name="$session_name"
   fi
 
-  if tmux has-session -t "$session_name" 2>/dev/null; then
-    if pgrep -f "tmux attach-session -t $session_name" >/dev/null; then
-      hyprctl dispatch focuswindow class:"$class_name"
-    else
-      exec kitty --class "$class_name" --title "$title_name" -e tmux attach-session -t "$session_name"
-    fi
+  if pgrep -af "kitty --class $class_name" >/dev/null; then
+    hyprctl dispatch focuswindow class:"$class_name"
+    tmux switch-client -t "$session_name"
   else
-    exec kitty --class "$class_name" --title "$title_name" -e tmuxp load "$session_name"
+    if tmux has-session -t "$session_name" 2>/dev/null; then
+      exec kitty --class "$class_name" --title "$title_name" -e tmux attach-session -t "$session_name"
+    else
+      exec kitty --class "$class_name" --title "$title_name" -e tmuxp load "$session_name"
+    fi
   fi
 }
 
