d6544bbbd7ddbb24e74aa2f3651b6ec3b16510e4
Augusto Coronel <anstrinn@disroot.org>
2025-03-29 17:24:02 -0300

    hypr: Add emacs script to open projects

---
+++ .config/hypr/scripts/emacs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+client_name="$1"
+client_address=$(hyprctl clients -j | jq -r ".[] | select(.title == \"$client_name\") | .address")
+
+if [ -n "$client_address" ]; then
+  hyprctl dispatch focuswindow address:"$client_address"
+  case "$client_name" in
+  daily-note)
+    emacsclient -c -r --eval "(progn (set-frame-parameter nil 'name \"$client_name\") (org-roam-dailies-goto-today))"
+    ;;
+  esac
+else
+  case "$client_name" in
+  daily-note)
+    emacsclient -c -a '' --eval "(progn (set-frame-parameter nil 'name \"$client_name\") (org-roam-dailies-goto-today))"
+    ;;
+  esac
+fi
