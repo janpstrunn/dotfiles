e2c2d830cab392f1efc16b1f4bcaf441156d5315
Janpstrunn <janpstrunn@disroot.org>
2025-03-29 10:20:23 -0300

    emacs: Add Emacs Doom config

---
+++ .config/doom/packages.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! beacon)
+(package! rainbow-mode)
+(package! dired-open)
+(package! org-auto-tangle)
+(package! dired-subtree)
+(package! org-modern)
+(package! olivetti)
+(package! reformatter)
+(package! pdf-tools)
