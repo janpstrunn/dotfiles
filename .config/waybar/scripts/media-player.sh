2ee4b55dfc7d28838acaf812520114411b03a9a8
Augusto Coronel <anstrinn@disroot.org>
2025-03-31 15:32:12 -0300

    waybar(refactor): Better media update

--- .config/waybar/scripts/media-player.sh
+++ .config/waybar/scripts/media-player.sh
@@ -1,15 +1,21 @@
 #!/bin/env bash
 
-TITLE=$(playerctl metadata --format '{{xesam:title}}' 2>/dev/null)
-ARTIST=$(playerctl metadata --format '{{xesam:artist}}' 2>/dev/null)
-STATUS=$(playerctl status 2>/dev/null)
+while true; do
+  STATUS=$(playerctl status 2>/dev/null)
 
-tooltip=$"Title: ${TITLE:-Unknown}"
-tooltip+=$"\nArtist: ${ARTIST:-Unknown}"
+  if [ -z "$STATUS" ]; then
+    echo "No media playing"
+    continue
+    sleep 10s
+  fi
 
-if [ -z "$STATUS" ]; then
-  echo "No media playing"
-  exit 1
-else
-  echo "{\"text\": \" ${TITLE:-No Title}\", \"tooltip\": \"$tooltip\"}"
-fi
+  TITLE=$(playerctl metadata --format '{{xesam:title}}' 2>/dev/null)
+  ARTIST=$(playerctl metadata --format '{{xesam:artist}}' 2>/dev/null)
+
+  tooltip=$"Title: ${TITLE:-Unknown}"
+  tooltip+=$"\nArtist: ${ARTIST:-Unknown}"
+
+  echo "{\"text\": \"${TITLE:-No Title}\", \"tooltip\": \"$tooltip\"}"
+
+  sleep 5s
+done
