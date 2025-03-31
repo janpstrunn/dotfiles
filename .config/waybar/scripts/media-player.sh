ff99568dc6484c91c146bff32f1368c83ed80671
Augusto Coronel <anstrinn@disroot.org>
2025-03-31 13:19:18 -0300

    waybar(scripts): Switch python media player to shell based

---
+++ .config/waybar/scripts/media-player.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+TITLE=$(playerctl metadata --format '{{xesam:title}}' 2>/dev/null)
+ARTIST=$(playerctl metadata --format '{{xesam:artist}}' 2>/dev/null)
+STATUS=$(playerctl status 2>/dev/null)
+
+tooltip="Status: ${STATUS:-Unknown}"
+tooltip+=$"\n'Title: ${TITLE:-Unknown}"
+tooltip+=$"\n'Artist: ${ARTIST:-Unknown}"
+
+if [ -z "$STATUS" ]; then
+  echo "No media playing"
+  exit 1
+else
+  echo "{\"text\": \" ${TITLE:-No Title}\", \"tooltip\": \"$tooltip\"}"
+fi
