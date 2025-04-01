58b802c849088155534bd834dee3d106c79c36f3
Augusto Coronel <anstrinn@disroot.org>
2025-04-01 16:26:25 -0300

    waybar: Set media interval to 3

--- .config/waybar/scripts/media-player.sh
+++ .config/waybar/scripts/media-player.sh
@@ -1,25 +1,23 @@
 #!/bin/env bash
 
-while true; do
-  STATUS=$(playerctl status 2>/dev/null)
+STATUS=$(playerctl status 2>/dev/null)
 
-  if [ -z "$STATUS" ]; then
-    echo "No media playing"
-    continue
-    sleep 10s
-  fi
+if [ -z "$STATUS" ]; then
+  echo "No media playing"
+  continue
+  sleep 10s
+fi
 
-  TITLE=$(playerctl metadata --format '{{xesam:title}}' 2>/dev/null)
-  ARTIST=$(playerctl metadata --format '{{xesam:artist}}' 2>/dev/null)
+TITLE=$(playerctl metadata --format '{{xesam:title}}' 2>/dev/null)
+ARTIST=$(playerctl metadata --format '{{xesam:artist}}' 2>/dev/null)
 
-  tooltip=$"Title: ${TITLE:-Unknown}"
-  tooltip+=$"\nArtist: ${ARTIST:-Unknown}"
+tooltip=$"Title: ${TITLE:-Unknown}"
+tooltip+=$"\nArtist: ${ARTIST:-Unknown}"
 
-  if [ "$STATUS" == "Playing" ]; then
-    echo "{\"text\": \" ${TITLE:-No media}\", \"tooltip\": \"$tooltip\"}"
-  else
-    echo "{\"text\": \" ${TITLE:-No media}\", \"tooltip\": \"$tooltip\"}"
-  fi
+if [ "$STATUS" == "Playing" ]; then
+  echo "{\"text\": \" ${TITLE:-No media}\", \"tooltip\": \"$tooltip\"}"
+else
+  echo "{\"text\": \" ${TITLE:-No media}\", \"tooltip\": \"$tooltip\"}"
+fi
 
-  sleep 5s
-done
+sleep 5s
