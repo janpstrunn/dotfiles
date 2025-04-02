42a89a7556de3d3a871ccbc2436e697c76725f69
Janpstrunn <janpstrunn@disroot.org>
2025-04-02 12:59:10 -0300

    emacs: Add function to insert webp from clipboard

---
+++ .config/doom/functions/insert-image.el
@@ -0,0 +1,8 @@
+(defun my/org-insert-clipboard-image ()
+  (interactive)
+  (let* ((script-path "./clipboard-to-webp.sh")
+         (fullpath (string-trim (shell-command-to-string script-path)))
+         (relpath (file-relative-name fullpath (file-name-directory (buffer-file-name)))))
+    (if (file-exists-p fullpath)
+        (insert (format "%s\n" relpath))
+      (message "Error: Image file was not created."))))
