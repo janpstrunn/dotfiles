e71c74375aef76a121e7b039873c5409531a97cc
Janpstrunn <janpstrunn@disroot.org>
2025-04-02 13:00:57 -0300

    emacs: Add snippet folders

---
+++ .config/doom/packages/yasnippet.el
@@ -0,0 +1,6 @@
+(setq yas-snippet-dirs
+      '("~/.config/doom/snippets"
+        "~/.local/share/yasnippet-snippets"
+        ))
+
+(yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
