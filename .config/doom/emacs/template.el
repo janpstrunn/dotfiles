dc9edb90a36e89bb1090b7a0c641c40764641d79
Janpstrunn <janpstrunn@disroot.org>
2025-04-03 09:41:55 -0300

    emacs: Split org config

--- .config/doom/emacs/template.el
+++ .config/doom/emacs/template.el
@@ -37,3 +37,38 @@
          (file+headline "~/org/notes.org" "Inbox")
          "* [%<%Y-%m-%d %a>] %^{Title}\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n%?"
          :prepend t)))
+
+
+(after! org-roam
+  (setq org-roam-capture-templates
+        '(("d" "default" plain "%?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("t" "timestamped entry" entry "%<%I:%M %p> %?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("e" "encrypted" plain "%?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org.gpg" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("l" "library" plain
+           (file "~/org/templates/library.org")
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t))))
+
+(after! org-roam-dailies
+  (setq org-roam-dailies-directory "~/org/journal/")
+  (setq org-roam-dailies-capture-templates
+        `(("t" "today" plain (file "~/org/templates/daily.org")
+           :target (file+head "daily/%<%Y-%m-%d>.org" "#+title: Daily: %<%Y-%m-%d>\n"))
+
+          ("d" "default" plain "** %<%I:%M %p> %?"
+           :if-new (file+head+olp "daily/%<%Y-%m-%d>.org" "#+title: Default: %<%Y-%m-%d>\n" ("Journal")))
+
+          ("w" "weekly" plain (file "~/org/templates/weekly.org")
+           :if-new (file+head "weekly/%<%Y-W%U>.org" "#+title: Week: %<%Y-W%U>\n"))
+
+          ("y" "yearly" plain (file "~/org/templates/monthly.org")
+           :if-new (file+head "%<%Y>.org" "#+title: Year: %<%Y>\n"))
+
+          ("m" "monthly" plain (file "~/org/templates/monthly.org")
+           :if-new (file+head "monthly/%<%B>, %<%Y>.org" "#+title: Month: %<%B>, %<%Y>\n")))))
