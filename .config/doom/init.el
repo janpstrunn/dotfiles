1edf2eed498972dbb43fb6f3db6b49488bb50bdc
Augusto Coronel <anstrinn@disroot.org>
2025-04-03 09:43:59 -0300

    emacs: Enable previous changes

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -99,7 +99,7 @@
        magit                      ; a git porcelain for Emacs
        ;;make                     ; run make tasks from Emacs
        ;;pass                     ; password manager for nerds
-       pdf                        ; pdf enhancements
+       ;; pdf                     ; pdf enhancements
        ;;prodigy                  ; FIXME managing external services & code builders
        ;;rgb                      ; creating color strings
        ;;taskrunner               ; taskrunner for all your projects
@@ -146,7 +146,7 @@
        lua                        ; one-based indices? one-based indices
        markdown                   ; writing docs for people to ignore
        ;;nim                      ; python + lisp at the speed of c
-       ;;nix                      ; I hereby declare "nix geht mehr!"
+       nix                        ; I hereby declare "nix geht mehr!"
        ;;ocaml                    ; an objective camel
        (org
         +journal                  ; enable org journal
