376ccfb1fded70f89580334401dbdf687493d66d
Augusto Coronel <anstrinn@disroot.org>
2025-04-06 19:53:56 -0300

    hypr: Adjust emacs startup

--- .config/hypr/scripts/emacs.sh
+++ .config/hypr/scripts/emacs.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
 client_name="$1"
-client_address=$(hyprctl clients -j | jq -r ".[] | select(.title == \"$client_name\") | .address")
+client_address=$(hyprctl clients -j | jq -r ".[] | select(.class == \"Emacs\") | .address")
 
 if [ -n "$client_address" ]; then
   hyprctl dispatch focuswindow address:"$client_address"
   case "$client_name" in
   daily-note)
-    emacsclient -c -r --eval "(progn (set-frame-parameter nil 'name \"$client_name\") (org-roam-dailies-goto-today))"
+    emacsclient -r --eval "(progn (set-frame-parameter nil 'name \"$client_name\") (org-roam-dailies-capture-today))"
+    ;;
+  extra)
+    emacsclient -c
     ;;
   esac
 else
   case "$client_name" in
   daily-note)
-    emacsclient -c -a '' --eval "(progn (set-frame-parameter nil 'name \"$client_name\") (org-roam-dailies-goto-today))"
+    emacsclient -r
+    ;;
+  extra)
+    emacsclient -c
     ;;
   esac
 fi
