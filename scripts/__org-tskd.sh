dc4dee8701bb408652243f97e6f8f59efcc1be04
Janpstrunn <janpstrunn@disroot.org>
2025-04-07 14:11:19 -0300

    feat: Does not require quoted dates, and add message on daily fail

--- scripts/__org-tskd.sh
+++ scripts/__org-tskd.sh
@@ -19,43 +19,46 @@ function tskd() {
   start_date=""
   end_date=""
 
-  case "$1" in
+  case "$*" in
   *-W[0-9][0-9]) # Matches YYYY-WXX
-    year=$(echo "$1" | awk -F '-W' '{print $1}')
-    week=$(echo "$1" | awk -F '-W' '{print $2}')
+    year=$(echo "$*" | awk -F '-W' '{print $1}')
+    week=$(echo "$*" | awk -F '-W' '{print $2}')
     printf -v week "%02d" "$week"
     start_date=$(date -d "$year-01-01 +$(((week - 1) * 7)) days -$(date -d "$year" +%u) days +1 day" '+%F')
     end_date=$(date -d "$start_date +6 days" '+%F')
     ;;
   [0-9][0-9][0-9][0-9]) # Matches YYYY
-    year=$1
+    year=$*
     start_date=$year-01-01
     end_date=$(date -d "$start_date +1 year" '+%F')
     ;;
   *-[0-9][0-9]-[0-9][0-9]) # Matches YYYY-MM-DD
-    raw_date="$1"
+    raw_date="$*"
     polish_date=$(echo "$raw_date" | sed 's/\.md//; s/th//')
     start_date=$(date -d "$polish_date" '+%Y-%m-%d')
+    [ ! -f "$ORG_DIR/$start_date.org" ] && {
+      echo "Given date does no have any done tasks!"
+      exit 0
+    }
     rg -HN "\* DONE " "$ORG_DIR/$start_date.org"
     exit 0
     ;;
   *)
-    if echo "$1" | grep -q "$month_list"; then
-      month_name=$(echo "$1" | awk -F ',' '{print $1}')
-      year=$(echo "$1" | awk -F ',' '{print $2}')
+    if echo "$*" | grep -q "$month_list"; then
+      month_name=$(echo "$*" | awk -F ',' '{print $1}')
+      year=$(echo "$*" | awk -F ',' '{print $2}')
       month_number=$(date -d "$month_name 1" '+%m')
-
       start_date="$year-$month_number-01"
       end_date=$(date -d "$start_date +1 month" '+%F')
     else
-      if [[ $1 = "" ]]; then # Today
+      if [[ $* = "" ]]; then # Today
         start_date=$(date '+%F')
         end_date=$(date -d "$start_date +1 day" '+%F')
       else # YYYY-MM
-        case "$1" in
+        case "$*" in
         *-[0-9][0-9]) # Matches YYYY-MM-DD
-          year=$(echo "$1" | awk -F '-' '{print $1}')
-          month=$(echo "$1" | awk -F '-' '{print $2}')
+          year=$(echo "$*" | awk -F '-' '{print $1}')
+          month=$(echo "$*" | awk -F '-' '{print $2}')
           start_date="$year"-"$month"-01
           end_date=$(date -d "$start_date +1 month" '+%F')
           ;;
