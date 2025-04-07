7e51fb1201220258b7afe784de2989a967ee6d20
Janpstrunn <janpstrunn@disroot.org>
2025-04-07 15:09:39 -0300

    emacs: Fix org-alert not working

--- .config/doom/packages/org-alert.el
+++ .config/doom/packages/org-alert.el
@@ -1,4 +1,8 @@
+(use-package org-alert
+  :ensure t)
 (setq alert-default-style 'libnotify)
 (setq org-alert-interval 300
       org-alert-notify-cutoff 60
+      org-alert-notification-title "Org Agenda"
       org-alert-notify-after-event-cutoff 10)
+(org-alert-enable)
