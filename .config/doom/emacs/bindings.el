97ccc31d285ca9fbce4ee44d1edee501cb714114
Augusto Coronel <anstrinn@disroot.org>
2025-04-10 14:34:57 -0300

    chore(emacs): Reduce org level sizes, and switch dirvish-side-peek to treemacs

--- .config/doom/emacs/bindings.el
+++ .config/doom/emacs/bindings.el
@@ -36,7 +36,7 @@
       (:prefix ("d" . "dired")
        :desc "Open dired" "d" #'dired
        :desc "Open dirvish" "D" #'dirvish
-       :desc "Open dirvish-side" "n" #'dirvish-side
+       ;; :desc "Open dirvish-side" "n" #'dirvish-side
        :desc "Dired jump to current" "j" #'dired-jump)
       (:after dired
               (:map dired-mode-map
