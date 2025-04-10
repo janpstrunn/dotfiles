26a55e5741ac8be253443975fd39cfc4166bc711
Janpstrunn <janpstrunn@disroot.org>
2025-04-10 14:35:33 -0300

    feat(emacs): Enable indent characters by default

---
+++ .config/doom/packages/indent-bars.el
@@ -0,0 +1,6 @@
+;;; ../../dotfiles/.config/doom/packages/indent-bars.el -*- lexical-binding: t; -*-
+
+(use-package indent-bars
+  :custom
+  (indent-bars-prefer-character t)
+  (indent-bars-no-stipple-char ?│))
