160936bcb94265c38e76668768989ae2857db270
Janpstrunn <janpstrunn@disroot.org>
2025-04-16 13:24:57 -0300

    feat(scripts): Add option to list and mount borg backups

--- scripts/__sync-external.sh
+++ scripts/__sync-external.sh
@@ -2,70 +2,105 @@
 
 # https://github.com/janpstrunn/dotfiles/blob/main/scripts/__sync-external.sh
 
-hostname="$(hostnamectl hostname)"
-now="$(date +%Y-%d-%H%M)"
+HOSTNAME="$(hostnamectl hostname)"
+NOW="$(date +%Y-%d-%H%M)"
+
+function help() {
+  cat <<EOF
+Sync Tool for External Drives
+Usage: $0 [command]
+Available commands:
+create                             - Use borg to backup
+list                               - List all borg backups
+mount                              - Mount borg
+rsync                              - Use rsync to sync source directory to dest directory
+Note: For borg, the source directory means the borg repository
+EOF
+}
 
 function get_drives() {
+  sourceonly=$1
   sourcedrive=$(ls /mnt/ | fzf --prompt "Choose directory you to copy from: " --preview 'eza -l /mnt/{}')
   if [ -z "$sourcedrive" ]; then
     echo "No source directory chosen!"
     exit 1
   fi
-  destdrive=$(ls /mnt/ | fzf --prompt "Choose directory to mirror to $sourcedrive: " --preview 'eza -l /mnt/{}')
-  if [ -z "$destdrive" ]; then
-    echo "No dest directory chosen!"
-    exit 1
+  if [ "$sourceonly" != "true" ]; then
+    destdrive=$(ls /mnt/ | fzf --prompt "Choose directory to mirror to $sourcedrive: " --preview 'eza -l /mnt/{}')
+    if [ -z "$destdrive" ]; then
+      echo "No dest directory chosen!"
+      exit 1
+    fi
   fi
 }
 
-function check_borg() {
-  ls /mnt/$sourcedrive | grep "nonce"
-  if [ "$?" -eq 0 ]; then
+function is_borg() {
+  if ls "/mnt/$sourcedrive" | grep "nonce"; then
     echo "This is a borg repository!"
     exit 0
   fi
-  ls /mnt/$destdrive | grep "nonce"
-  if [ "$?" -eq 0 ]; then
+  if ls "/mnt/$destdrive" | grep "nonce"; then
     echo "This is a borg repository!"
     exit 0
   fi
 }
 
 function sync_external() {
+  # beelzebub is a directory I do not want to overwrite accidentally
+  # better safe than sorry
   if [ "$destdrive" = "beelzebub" ]; then
     echo "Caution!"
     echo "The beelzebub directory have been selected as destdrive"
-    exit 0
+    exit 1
   else
-    rsync --progress -avh --delete /mnt/"$sourcedrive/" /mnt/"$destdrive/"
+    rsync --progress -avh --delete "/mnt/$sourcedrive/" /mnt/"$destdrive/"
   fi
 }
 
-function run_borg() {
-  borg create --stats --progress /mnt/"$sourcedrive"::"$hostname"-"$now" "$destdrive/"
+function create_backup() {
+  borg create --stats --progress "/mnt/$sourcedrive::$HOSTNAME-$NOW" "/mnt/$destdrive/"
 }
 
-function help() {
-  cat <<EOF
-Sync Tool for External Drives
-Usage: $0 [option]
-Available options:
--b                            - Use borg to backup
--s                            - Use rsync to sync source directory to dest directory
-For borg, the source directory means the borg repository
-EOF
+function list_borg() {
+  borg list "/mnt/$sourcedrive"
 }
 
-if [[ "$1" == "-b" ]]; then
-  get_drives
-  run_borg
+function mount_borg() {
+  destdrive="$sourcedrive"_mount
+  [[ ! -d "$destdrive" ]] && {
+    mkdir -p "$destdrive"
+    [[ ! -d "$destdrive" ]] && {
+      echo "Failed to create directory!"
+      exit 1
+    }
+  }
+  borg mount "/mnt/$sourcedrive" "/mnt/$destdrive"
+}
+
+case "$1" in
+mount)
+  get_drives true # Source Only
+  mount_borg
   exit 0
-elif [[ "$1" == "-s" ]]; then
+  ;;
+list)
+  get_drives true # Source Only
+  list_borg
+  exit 0
+  ;;
+create)
   get_drives
-  check_borg
+  create_backup
+  exit 0
+  ;;
+rsync)
+  get_drives
+  is_borg
   sync_external
   exit 0
-else
+  ;;
+*)
   help
   exit 0
-fi
+  ;;
+esac
