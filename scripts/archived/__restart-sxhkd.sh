2eb59b7c1cc0594a679c35ee7b08ce7090ba2e06
Janpstrunn <janpstrunn@disroot.org>
2025-04-17 14:54:27 -0300

    deprecate(scripts): Archive unused scripts

---
+++ scripts/archived/__restart-sxhkd.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# https://github.com/janpstrunn/dotfiles/blob/main/scripts/__restart-sxhkd.sh
+
+pkill sxhkd
+tmux kill-session -t key
+tmuxp load key -d
---
+++ scripts/archived/__switch_to_tui.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# https://github.com/janpstrunn/dotfiles/blob/main/scripts/__switch_to_tui.sh
+
+set -eo pipefail
+
+TASK_DESCRIPTION="$1"
+
+if [ -z "$TASK_DESCRIPTION" ]; then
+  echo "No description is given. Exitting..."
+  exit 1
+fi
+
+if ! tmux has-session -t "task"; then
+  tmuxp load "task" -d
+  sleep 0.1
+fi
+
+tmux send-keys -t task:taskwarrior "/description:$TASK_DESCRIPTION" C-m
+
+tmux switch-client -t task
