513ff21ccdcf22951e6416c5a5ecd6dc75905d7b
Janpstrunn <janpstrunn@disroot.org>
2025-04-17 13:37:37 -0300

    feat(scripts): Add OTP support

--- scripts/__filen.sh
+++ scripts/__filen.sh
@@ -2,12 +2,14 @@
 
 EMAIL=$(pass personal/filen | sed -n 's/^email=//p')
 PASSWORD=$(pass personal/filen | head -n 1)
+OTP=$(pass otp personal/filen)
 W_USER=user
 W_PASSWORD=secret
 
 podman run -d -p 8080:80 filen/cli:latest \
   --email "$EMAIL" \
   --password "$PASSWORD" \
+  --two-factor-code "$OTP" \
   webdav \
   --w-user "$W_USER" \
   --w-password "$W_PASSWORD"
