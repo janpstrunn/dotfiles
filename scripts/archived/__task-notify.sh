027b451b43a2093b08f0700dd1880ed90dca6a66
Augusto Coronel <anstrinn@disroot.org>
2025-04-17 14:54:27 -0300

    deprecate(scripts): Archive unused scripts

---
+++ scripts/archived/__task-notify.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# https://github.com/janpstrunn/dotfiles/blob/main/scripts/__task-notify.sh
+
+TMP_FILE="/tmp/taskwarrior_notified"
+touch "$TMP_FILE"
+
+notify() {
+  local ID DESC DUE_TIME DUE_TIME_EPOCH CURRENT_TIME SLEEP_TIME LAST_NOTIFIED FORMATTED_TIME TYPE
+  ID=$(jq -r '.id' <<<"$TASK")
+  DESC=$(jq -r '.description' <<<"$TASK")
+  DUE_TIME=$(jq -r '.due' <<<"$TASK")
+
+  [[ -z "$DUE_TIME" || "$DUE_TIME" == "null" ]] && return
+
+  FORMATTED_TIME=$(echo "$DUE_TIME" | sed -E 's/(.{4})(.{2})(.{2})T(.{2})(.{2})(.{2})Z/\4:\5/')
+  DUE_TIME_EPOCH=$(echo "$DUE_TIME" | sed -E 's/(.{4})(.{2})(.{2})T(.{2})(.{2})(.{2})Z/\1-\2-\3 \4:\5:\6/' | xargs -I{} date -u -d "{}" +%s)
+
+  CURRENT_TIME=$(date +%s)
+  SLEEP_TIME=$((DUE_TIME_EPOCH - CURRENT_TIME))
+
+  if ((SLEEP_TIME <= 0)); then
+    TYPE="overdue"
+  else
+    TYPE="due"
+  fi
+
+  LAST_NOTIFIED=$(awk -v id="$ID" '$1 == id {print $2}' "$TMP_FILE")
+  [[ -n "$LAST_NOTIFIED" && $((CURRENT_TIME - LAST_NOTIFIED)) -lt 1800 ]] && return
+
+  sed -i "/^$ID /d" "$TMP_FILE"
+  echo "$ID $CURRENT_TIME" >>"$TMP_FILE"
+
+  notify-send -a "taskwarrior" "Task is $TYPE" "$DESC at $FORMATTED_TIME"
+}
+
+while true; do
+  task status:pending +READY due:today sort:due export | jq -c '.[]' | while read -r TASK; do
+    notify
+  done
+
+  task status:pending +READY due.before:today sort:due export | jq -c '.[]' | while read -r TASK; do
+    notify
+  done
+
+  sleep 60
+done
---
+++ scripts/archived/__tmux-task.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# https://github.com/janpstrunn/dotfiles/blob/main/scripts/__tmux-task.sh
+
+ENV_FILE="$HOME/.scriptenv"
+
+if [ -z "$ENV_FILE" ]; then
+  echo ".scriptenv is missing at $HOME!"
+  return 1
+fi
+
+source "$ENV_FILE"
+
+if [ -z "$SHELL" ] || [ -z "$SCRIPTS" ]; then
+  echo "Error: SHELL or SCRIPTS env at .scriptenv not found"
+  exit 1
+fi
+
+if ! command -v task &>/dev/null; then
+  echo "task could not be found. Please install it."
+  exit 1
+fi
+
+current_command=$(tmux display-message -p "#{pane_current_command}")
+
+if [[ "$current_command" == "nvim" ]]; then
+  tmux send-keys -t 0 ":call writefile([fnamemodify(expand('%:p'), ':t')], '/tmp/filename_output.txt')" C-m
+  sleep 0.1
+  file_name=$(cat /tmp/filename_output.txt)
+  file_name=$(echo "$file_name" | sed 's/\.md//; s/th//')
+
+  if [[ -n "$file_name" ]]; then
+    tmux splitw -v \
+      "$SHELL" -c "$SCRIPTS/__tskd.sh \"$file_name\" tmux && exec $SHELL"
+  else
+    echo "Could not retrieve the file name."
+  fi
+else
+  echo "Not in nvim."
+fi
