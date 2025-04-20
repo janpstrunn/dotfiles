0e23abb1959f052ef712b07bd56bb6813f10605e
Janpstrunn <janpstrunn@disroot.org>
2025-04-20 10:50:21 -0300

    fix(scripts): Source .scriptenv

--- scripts/exit.sh
+++ scripts/exit.sh
@@ -2,6 +2,8 @@
 
 # https://github.com/user/dotfiles/blob/main/scripts/exit.sh
 
+source "$HOME/.scriptenv"
+
 function umount_dir() {
   if [ -z "$(ls -A "$BEELZEBUB")" ]; then
     echo "Directory '$BEELZEBUB' is empty"
