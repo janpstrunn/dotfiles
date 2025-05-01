89d2220e2f7bd59d558ed0a3127a60383395fee2
Janpstrunn <janpstrunn@disroot.org>
2025-05-01 19:05:14 -0300

    chore(emacs): Attempt to improve spelling

---
+++ .config/doom/emacs/spell.el
@@ -0,0 +1,20 @@
+;;; ../../dotfiles/.config/doom/emacs/spell.el -*- lexical-binding: t; -*-
+
+
+(defun switch-to-english ()
+  (interactive)
+  (setq ispell-dictionary "en")
+  (message "Switched to English dictionary"))
+
+(defun switch-to-portuguese ()
+  (interactive)
+  (setq ispell-dictionary "pt_BR")
+  (message "Switched to Portuguese dictionary"))
+
+(global-set-key (kbd "C-c se") 'switch-to-english)
+(global-set-key (kbd "C-c sp") 'switch-to-portuguese)
+
+(remove-hook 'markdown-mode-hook #'spell-fu-mode)
+(remove-hook 'org-mode-hook #'spell-fu-mode)
+(remove-hook 'prog-mode-hook #'spell-fu-mode)
+(remove-hook 'text-mode-hook #'spell-fu-mode)
