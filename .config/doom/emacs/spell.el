7259efc8770a1b984fab8c718d9f22e0095bd96f
Augusto Coronel <anstrinn@disroot.org>
2025-05-02 18:14:09 -0300

    feat(emacs): Enable proper spelling for spell-fu

--- .config/doom/emacs/spell.el
+++ .config/doom/emacs/spell.el
@@ -1,6 +1,5 @@
 ;;; ../../dotfiles/.config/doom/emacs/spell.el -*- lexical-binding: t; -*-
 
-
 (defun switch-to-english ()
   (interactive)
   (setq ispell-dictionary "en")
@@ -18,3 +17,10 @@
 (remove-hook 'org-mode-hook #'spell-fu-mode)
 (remove-hook 'prog-mode-hook #'spell-fu-mode)
 (remove-hook 'text-mode-hook #'spell-fu-mode)
+
+(add-hook 'spell-fu-mode-hook
+          (lambda ()
+            (spell-fu-dictionary-add (spell-fu-get-ispell-dictionary "en"))
+            (spell-fu-dictionary-add (spell-fu-get-ispell-dictionary "pt_BR"))
+            (spell-fu-dictionary-add
+             (spell-fu-get-personal-dictionary "en-personal" "/home/janpstrunn/.aspell.pws"))))
