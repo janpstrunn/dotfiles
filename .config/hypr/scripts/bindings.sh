70bc7dc43aaa0097d74c5741001f434c82b20d10
Janpstrunn <janpstrunn@disroot.org>
2025-05-03 14:40:00 -0300

    feat(hypr): Improve the binding searcher

---
+++ .config/hypr/scripts/bindings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# "Show all Hyprland Keybindings"
+
+function _rofi() {
+  rofi -dmenu -i -no-levenshtein-sort -width 1000
+}
+
+rg "bind" <"$HOME/.config/hypr/modules/keybinds.conf" | sed "s/^[^=]*=//; s/exec,[^#]*#/#/" | _rofi
