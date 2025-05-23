e7252ff343df8d9f68964402e161823004bd5674
Augusto Coronel <anstrinn@disroot.org>
2025-05-23 13:18:10 -0300

    feat(scripts): Add syncing to a new directory
    - Mirror $STOW_DIR and $HOME directories

--- scripts/__home-sync.sh
+++ scripts/__home-sync.sh
@@ -1,13 +1,48 @@
 #!/usr/bin/env bash
 
 # https://github.com/janpstrunn/dotfiles/blob/main/scripts/__home-sync.sh
+# Neostow Reference: https://github.com/janpstrunn/neostow/blob/main/src/neostow
+
+# 1. "Make a $HOME backup using rsync"
+# 2. "Make a $STOW_DIR backup using rsync"
+
+RSYNC_FILE="$HOME/.rsync-ignore"
+STOW_DIR="$HOME/stow/" # Special directory, which I use neostow
+DIR=$1
+
+if [ ! -d "$HOME/home" ]; then
+  echo "Please, create a new directory or symlink at:"
+  echo "$HOME/home"
+  exit 1
+fi
+
+if [ ! -f "$RSYNC_FILE" ]; then
+  echo "You do not have a .rsync-ignore file"
+  echo "Create one at $RSYNC_FILE. It works like .gitignore"
+fi
+
+function sync_main() {
+  # "Mirror the $HOME to $HOME/home/"
+  rsync --progress -ah --delete --exclude-from=$HOME/.rsync-ignore "$HOME/" "$HOME/home/"
+}
+
+function sync_stow() {
+  # "Mirror the $STOW_DIR to specified DIR
+  if [ -z "$DIR" ]; then
+    DIR=$(realpath /mnt/**/stow/)
+    if [ ! -d "$DIR" ]; then
+      echo "$DIR does not exist."
+      return 1
+    fi
+  fi
+  rsync --progress -ah --delete "$STOW_DIR" "$DIR"
+}
 
 function main() {
-  if [ ! -d "$HOME/home" ]; then
-    echo "Please, create a new directory at:"
-    echo "$HOME/home"
+  sync_main
+  if [ -d "$STOW_DIR" ]; then
+    sync_stow
   fi
-  rsync --progress -ah --delete --exclude-from=$HOME/.rsync-ignore "$HOME/" "$HOME/home/"
 }
 
 main
