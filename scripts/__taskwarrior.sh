2b8cd4feafa2a14093f7419ec1554942b3a57f54
Augusto Coronel <anstrinn@disroot.org>
2025-06-03 13:32:40 -0300

    feat(scripts): Add several features
    - Add edit command
    - Interactively select id for editing and modifing tasks
    - Add help function
    - Increase task limit to 5 on the `k` command
    - Rename `clean` to `purge`

--- scripts/__taskwarrior.sh
+++ scripts/__taskwarrior.sh
@@ -3,6 +3,29 @@
 TASK_DIR=$HOME/.task/
 TASK_GIT_DIR=$HOME/.task-git/
 
+function help() {
+  cat <<EOF
+Usage: tt [command] [args]
+Options:
+-i <id>          - Define an ID
+Commands
+a [args]         - Add
+d [args]         - Due today
+e -i <id>        - Edit task
+git              - Commit changes
+k                - Five most urgent tasks
+m -i <id> [args] - Modify task
+purge            - Purge all deleted tasks
+q [args]         - +Quick tasks
+t                - Taskwarrior TUI
+v                - VIT
+EOF
+}
+
+function get_id() {
+  task status:pending export | jq -r '.[] | "\(.id) | \(.description)"' | fzf | awk -F " " '{print $1}'
+}
+
 function commands() {
   case "$command" in
   a)
@@ -11,19 +34,40 @@ function commands() {
   d)
     task list due:today "$args"
     ;;
+  e)
+    if [ -z "$id" ]; then
+      id=$(get_id)
+    fi
+    task "$id" edit
+    ;;
   q)
     task add +quick "$args"
     ;;
   m)
+    if [ -z "$args" ]; then
+      echo "No arguments used!"
+      echo "Use: tt m <args>"
+      exit 1
+    fi
+    if [ -z "$id" ]; then
+      id=$(get_id)
+    fi
     task "$id" modify "$args"
     ;;
   t)
     taskwarrior-tui
     ;;
-  k)
-    task limit:3 "$args"
+  v)
+    vit
     ;;
-  clean)
+  help)
+    help
+    exit 0
+    ;;
+  k)
+    task limit:5 "$args"
+    ;;
+  purge)
     task +DELETED purge
     ;;
   git)
@@ -32,20 +76,32 @@ function commands() {
     ;;
   *)
     if [[ $(uname -a) =~ "Android" ]]; then
-      task termux "$args"
+      task termux "$command" "$args"
     else
-      task list "$args"
+      task list "$command" "$args"
     fi
     ;;
   esac
 }
 
+while getopts ":i:" opt; do
+  case "$opt" in
+  i)
+    id=$OPTARG
+    ;;
+  ?)
+    echo "Error: Invalid option '-$OPTARG'" >&2
+    exit 1
+    ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
 function main() {
   command=$1
   shift
-  id=$1
-  shift
-  args=$@
+  args="$@"
   commands
 }
 
