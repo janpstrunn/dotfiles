7a7fe81bb40c47955680c06014e98977c36c244d
Augusto Coronel <anstrinn@disroot.org>
2025-06-06 14:48:52 -0300

    fix(waybar): Taskwarrior not working properly
    - If the ORG_DIR did not existed, it was echoing a "~", which broke the
    json format

--- .config/waybar/scripts/task.sh
+++ .config/waybar/scripts/task.sh
@@ -6,10 +6,6 @@ ORG_DIR="$HOME/org"
 # taskmode="org"
 taskmode="taskwarrior"
 
-if [ ! -d "$ORG_DIR" ]; then
-  echo "~"
-fi
-
 case "$1" in
 "all") echo "all" >"$modefile" ;;
 "today") echo "today" >"$modefile" ;;
@@ -20,12 +16,15 @@ if [ "$taskmode" == "taskwarrior" ]; then
     if [ "$(cat "$modefile")" = "all" ]; then
       tasks=$(task status:pending count)
     elif [ "$(cat "$modefile")" = "today" ]; then
-      tasks=$(task status:pending due:today count)
+      tasks=$(task status:pending due.before:tomorrow count)
     fi
   else
     tasks=$(task status:pending count)
   fi
 elif [ "$taskmode" == "org" ]; then
+  if [ ! -d "$ORG_DIR" ]; then
+    exit 1
+  fi
   if [ -f "$modefile" ]; then
     if [ "$(cat "$modefile")" = "all" ]; then
       tasks=$(rg --max-depth 2 -o "^\*+\s+TODO" "$ORG_DIR" --no-filename | wc -l)
