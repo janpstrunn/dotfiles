42c63b89db5dfe66258cbc0509194fae51b86cba
Augusto Coronel <anstrinn@disroot.org>
2025-06-06 15:04:16 -0300

    feat(scripts): Add verbose, and improve output readibility

--- scripts/__obsidian-helper.sh
+++ scripts/__obsidian-helper.sh
@@ -7,18 +7,38 @@ source "$HOME/.scriptenv"
 function main() {
   command="$1"
   find "$OBSIDIAN" -mindepth 1 -maxdepth 1 -type d | while read -r vault; do
-    echo "Working on $vault"
     if [ -d "$vault/.git" ]; then
+      echo "Working on $vault"
       if [ "$command" == "commit" ]; then
         git -C "$vault" add .
         git -C "$vault" commit -a -m "$(date +%F)"
+      elif [ "$command" == "" ]; then
+        git -C "$vault" status
       else
         git -C "$vault" "$command"
       fi
+      echo
     else
-      echo "Vault: $vault. Not a Git repository."
+      if [ "$VERBOSE" = "true" ]; then
+        echo "Vault: $vault. Not a Git repository."
+      fi
+      continue
     fi
   done
 }
+
+while getopts ":v" opt; do
+  case "$opt" in
+  v)
+    VERBOSE=true
+    ;;
+  ?)
+    text "$RED" "Error: Invalid option '-$OPTARG'" >&2
+    exit 1
+    ;;
+  esac
+done
+
+shift $((OPTIND - 1))
 
 main $@
