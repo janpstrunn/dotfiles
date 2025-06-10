880cee4f7787724850956b78f2da33365f7f8825
Janpstrunn <janpstrunn@disroot.org>
2025-06-10 19:36:55 -0300

    feat(yazi): Add yazi config files and elegantvagrant theme
    - tmTheme is temporary catpuccin

---
+++ .config/yazi/init.lua
@@ -0,0 +1,48 @@
+require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false, mode="dir" },
+	persist = "none",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	custom_desc_input = false,
+	notify = {
+		enable = false,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
+
+require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("#7c5cff"),
+		" ",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("#7c7cff"),
+		" ",
+	}
+end, 500, Status.RIGHT)
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("#46d9ff")
+end, 500, Header.LEFT)
