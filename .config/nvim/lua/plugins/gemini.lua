e70f4a4535ba39d932a853e47415042668ad0d9c
Augusto Coronel <anstrinn@disroot.org>
2025-06-10 19:38:55 -0300

    feat(nvim): Customize gemini blacklist and custom prompts

--- .config/nvim/lua/plugins/gemini.lua
+++ .config/nvim/lua/plugins/gemini.lua
@@ -13,4 +13,31 @@ return {
   config = function()
     require("gemini").setup()
   end,
+  opts = {
+    completion = {
+      enabled = true,
+      blacklist_filetypes = { 'help', 'qf', 'json', 'yaml', 'toml', 'md' },
+      blacklist_filenames = { '.secrets' },
+      completion_delay = 600,
+      move_cursor_end = false,
+      insert_result_key = '<S-Tab>',
+    },
+  -- instruction = {
+  --   enabled = true,
+  --   prompts = {
+  --     {
+  --       name = 'CUSTOM PROMPT',
+  --       command_name = 'GeminiCustomPrompt',
+  --       menu = 'Custom Prompt 🚀',
+  --       get_prompt = function(lines, bufnr)
+  --         local code = vim.fn.join(lines, '\n')
+  --         local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+  --         local prompt = 'Context:\n\n```%s\n%s\n```\n\n'
+  --             .. 'Write your custom prompt\n'
+  --         return string.format(prompt, filetype, code)
+  --       end,
+  --     },
+  --   }
+  -- },
+  },
 }
