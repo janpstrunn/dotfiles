8fdf6be17713bc08ff402d8ce83e257437cc8ff6
Augusto Coronel <anstrinn@disroot.org>
2025-06-18 16:25:19 -0300

    chore(scripts): Change port to 9090

--- scripts/__filen.sh
+++ scripts/__filen.sh
@@ -6,7 +6,7 @@ OTP=$(pass otp personal/filen)
 W_USER=user
 W_PASSWORD=secret
 
-podman run -d -p 8080:80 filen/cli:latest \
+podman run -d -p 9090:80 filen/cli:latest \
   --email "$EMAIL" \
   --password "$PASSWORD" \
   --two-factor-code "$OTP" \
