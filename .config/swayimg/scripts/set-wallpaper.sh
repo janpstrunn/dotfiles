5ae14409c0df7c95c41cb953747f4d0eba3de78e
Augusto Coronel <anstrinn@disroot.org>
2025-06-22 18:38:24 -0300

    fix(swayimg): Fix swayimg scripts

--- .config/swayimg/scripts/set-wallpaper.sh
+++ .config/swayimg/scripts/set-wallpaper.sh
@@ -1,11 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 file=$1
-
 swww img --transition-type=fade "$file"
-
-if [ ! -f $filename ]; then
-	touch $filename
-fi
-
-echo "$file" >$HOME/.config/wall && exit
