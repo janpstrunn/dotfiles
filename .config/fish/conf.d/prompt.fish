d8cb8c6bf78889aac82cc053f95baede6c221985
Augusto Coronel <anstrinn@disroot.org>
2025-07-22 13:20:56 -0300

    chore(fish): Use builtin function to display vcs info

--- .config/fish/conf.d/prompt.fish
+++ .config/fish/conf.d/prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     set_color "#7c5cff"
-    echo "$(prompt_pwd --full-length-dirs 2)$(set_color normal)$(set_color "#f4f113") $(git branch 2>/dev/null | sed -n '/^[*]/s///p')"
+    echo "$(prompt_pwd --full-length-dirs 2)$(set_color normal)$(set_color "#f4f113") $(fish_git_prompt)"
     set_color "#7c5cff"
     echo '❯' (set_color normal)
 end
